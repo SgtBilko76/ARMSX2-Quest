SgtBilko76/ARMSX2-Quest

commit 86b44495584224d5b3faf73683e0c0d7a470b794

* Removed a completely unnecessary use of xSmartJump in the SLT instruction generation * Reduced the amount of console log spamming when hitting F4/Tab/etc.
git-svn-id: http://pcsx2.googlecode.com/svn/trunk@3398 96395faa-99c1-11dd-bbfe-3dabce05a288

pcsx2/gui/AppCoreThread.cpp
@@ -190,7 +190,7 @@ void AppCoreThread::OnResumeReady()
 // Load Game Settings found in database
 // (game fixes, round modes, clamp modes, etc...)
 // Returns number of gamefixes set
-static int loadGameSettings(Pcsx2Config& dest, const Game_Data& game) {
+static int loadGameSettings(Pcsx2Config& dest, const Game_Data& game, bool verbose = true) {
 	if( !game.IsOk() ) return 0;
 
 	int  gf  = 0;
@@ -200,7 +200,7 @@ static int loadGameSettings(Pcsx2Config& dest, const Game_Data& game) {
 		SSE_RoundMode eeRM = (SSE_RoundMode)game.getInt("eeRoundMode");
 		if (EnumIsValid(eeRM))
 		{
-			Console.WriteLn("(GameDB) Changing EE/FPU roundmode to %d [%s]", eeRM, EnumToString(eeRM));
+			if(verbose) Console.WriteLn("(GameDB) Changing EE/FPU roundmode to %d [%s]", eeRM, EnumToString(eeRM));
 			dest.Cpu.sseMXCSR.SetRoundMode(eeRM);
 			++gf;
 		}
@@ -211,7 +211,7 @@ static int loadGameSettings(Pcsx2Config& dest, const Game_Data& game) {
 		SSE_RoundMode vuRM = (SSE_RoundMode)game.getInt("vuRoundMode");
 		if (EnumIsValid(vuRM))
 		{
-			Console.WriteLn("(GameDB) Changing VU0/VU1 roundmode to %d [%s]", vuRM, EnumToString(vuRM));
+			if(verbose) Console.WriteLn("(GameDB) Changing VU0/VU1 roundmode to %d [%s]", vuRM, EnumToString(vuRM));
 			dest.Cpu.sseVUMXCSR.SetRoundMode(vuRM);
 			++gf;
 		}
@@ -219,7 +219,7 @@ static int loadGameSettings(Pcsx2Config& dest, const Game_Data& game) {
 
 	if (game.keyExists("eeClampMode")) {
 		int clampMode = game.getInt("eeClampMode");
-		Console.WriteLn("(GameDB) Changing EE/FPU clamp mode [mode=%d]", clampMode);
+		if(verbose) Console.WriteLn("(GameDB) Changing EE/FPU clamp mode [mode=%d]", clampMode);
 		dest.Recompiler.fpuOverflow			= (clampMode >= 1);
 		dest.Recompiler.fpuExtraOverflow	= (clampMode >= 2);
 		dest.Recompiler.fpuFullMode			= (clampMode >= 3);
@@ -228,7 +228,7 @@ static int loadGameSettings(Pcsx2Config& dest, const Game_Data& game) {
 
 	if (game.keyExists("vuClampMode")) {
 		int clampMode = game.getInt("vuClampMode");
-		Console.WriteLn("(GameDB) Changing VU0/VU1 clamp mode [mode=%d]", clampMode);
+		if(verbose) Console.WriteLn("(GameDB) Changing VU0/VU1 clamp mode [mode=%d]", clampMode);
 		dest.Recompiler.vuOverflow			= (clampMode >= 1);
 		dest.Recompiler.vuExtraOverflow		= (clampMode >= 2);
 		dest.Recompiler.vuSignOverflow		= (clampMode >= 3);
@@ -244,7 +244,7 @@ static int loadGameSettings(Pcsx2Config& dest, const Game_Data& game) {
 		{
 			bool enableIt = game.getBool(key);
 			dest.Gamefixes.Set(id, enableIt);
-			Console.WriteLn(L"(GameDB) %s Gamefix: " + key, enableIt ? L"Enabled" : L"Disabled" );
+			if(verbose) Console.WriteLn(L"(GameDB) %s Gamefix: " + key, enableIt ? L"Enabled" : L"Disabled" );
 			gf++;
 		}
 	}
@@ -254,6 +254,11 @@ static int loadGameSettings(Pcsx2Config& dest, const Game_Data& game) {
 
 void AppCoreThread::ApplySettings( const Pcsx2Config& src )
 {
+	// Used to track the current game serial/id, and used to disable verbose logging of
+	// applied patches if the game info hasn't changed.  (avoids spam when suspending/resuming
+	// or using TAB or other things).
+	static wxString curGameKey;
+
 	// 'fixup' is the EmuConfig we're going to upload to the emulator, which very well may
 	// differ from the user-configured EmuConfig settings.  So we make a copy here and then
 	// we apply the commandline overrides and database gamefixes, and then upload 'fixup'
@@ -289,10 +294,13 @@ void AppCoreThread::ApplySettings( const Pcsx2Config& src )
 	if (ElfCRC) gameCRC.Printf( L"%8.8x", ElfCRC );
 	if (!DiscSerial.IsEmpty()) gameSerial = L" [" + DiscSerial  + L"]";
 
+	const wxString newGameKey( SysGetDiscID() );
+	const bool verbose( newGameKey != curGameKey );
+
 	if (IGameDatabase* GameDB = AppHost_GetGameDatabase() )
 	{
 		Game_Data game;
-		if (GameDB->findGame(game, SysGetDiscID())) {
+		if (GameDB->findGame(game, curGameKey)) {
 			int compat = game.getInt("Compat");
 			gameName   = game.getString("Name");
 			gameName  += L" (" + game.getString("Region") + L")";
@@ -302,8 +310,9 @@ void AppCoreThread::ApplySettings( const Pcsx2Config& src )
 		if (EmuConfig.EnablePatches) {
 			if (int patches = InitPatches(gameCRC, game)) {
 				gamePatch.Printf(L" [%d Patches]", patches);
+				if (verbose) Console.WriteLn(Color_Green, "(GameDB) Patches Loaded: %d", patches);
 			}
-			if (int fixes = loadGameSettings(fixup, game)) {
+			if (int fixes = loadGameSettings(fixup, game, verbose)) {
 				gameFixes.Printf(L" [%d Fixes]", fixes);
 			}
 		}
@@ -314,6 +323,8 @@ void AppCoreThread::ApplySettings( const Pcsx2Config& src )
 			gameCheats.Printf(L" [%d Cheats]", cheats);
 		}
 	}
+
+	curGameKey = newGameKey;
 
 	Console.SetTitle(gameName+gameSerial+gameCompat+gameFixes+gamePatch+gameCheats);
 

pcsx2/Patch.cpp
@@ -149,12 +149,7 @@ int InitPatches(const wxString& crc, const Game_Data& game)
 		}
 	}
 	
-	if (patchFound) {
-		Console.WriteLn(Color_Green, "Patch found in the Database!");
-		TrimPatches(patch);
-		Console.WriteLn(Color_Green, "Patches Loaded: %d", patchnumber);
-	}
-	else Console.WriteLn(Color_Gray, "No patch for this game in the Database.");
+	if (patchFound) TrimPatches(patch);
 	
 	return patchnumber;
 }

pcsx2/x86/ix86-32/iR5900Arit.cpp
@@ -587,19 +587,19 @@ void recSLTs_const(int info, int sign, int st)
 	GPR_reg64 cval = g_cpuConstRegs[st ? _Rt_ : _Rs_];
 
 	xMOV(eax, 1);
-	{
-		xCMP(ptr32[&cpuRegs.GPR.r[st ? _Rs_ : _Rt_].UL[1]], cval.UL[1]);
-		xSmartJump pass1(st ? (sign ? Jcc_Less : Jcc_Below) : (sign ? Jcc_Greater : Jcc_Above));
-		xForwardJump<u8> fail(st ? (sign ? Jcc_Greater : Jcc_Above) : (sign ? Jcc_Less : Jcc_Below));
-		{
-			xCMP(ptr32[&cpuRegs.GPR.r[st ? _Rs_ : _Rt_].UL[0]], cval.UL[0]);
-			xForwardJump<u8> pass2(st ? Jcc_Below : Jcc_Above);
 
-			fail.SetTarget();
-			xMOV(eax, 0);
-			pass2.SetTarget();
-		}
+	xCMP(ptr32[&cpuRegs.GPR.r[st ? _Rs_ : _Rt_].UL[1]], cval.UL[1]);
+	xForwardJump8 pass1(st ? (sign ? Jcc_Less : Jcc_Below) : (sign ? Jcc_Greater : Jcc_Above));
+	xForwardJump8 fail(st ? (sign ? Jcc_Greater : Jcc_Above) : (sign ? Jcc_Less : Jcc_Below));
+	{
+		xCMP(ptr32[&cpuRegs.GPR.r[st ? _Rs_ : _Rt_].UL[0]], cval.UL[0]);
+		xForwardJump8 pass2(st ? Jcc_Below : Jcc_Above);
+
+		fail.SetTarget();
+		xMOV(eax, 0);
+		pass2.SetTarget();
 	}
+	pass1.SetTarget();
 
 	xMOV(ptr32[&cpuRegs.GPR.r[_Rd_].UL[0]], eax);
 	xMOV(ptr32[&cpuRegs.GPR.r[_Rd_].UL[1]], 0);
@@ -610,21 +610,21 @@ void recSLTs_(int info, int sign)
 	pxAssert( !(info & PROCESS_EE_XMM) );
 
 	xMOV(eax, 1);
-	{
-		xMOV(edx, ptr32[&cpuRegs.GPR.r[_Rs_].UL[1]]);
-		xCMP(edx, ptr32[&cpuRegs.GPR.r[_Rt_].UL[1]]);
-		xSmartJump pass1(sign ? Jcc_Less : Jcc_Below);
-		xForwardJump<u8> fail(sign ? Jcc_Greater : Jcc_Above);
-		{
-			xMOV(edx, ptr32[&cpuRegs.GPR.r[_Rs_].UL[0]]);
-			xCMP(edx, ptr32[&cpuRegs.GPR.r[_Rt_].UL[0]]);
-			xForwardJump<u8> pass2(Jcc_Below);
 
-			fail.SetTarget();
-			xMOV(eax, 0);
-			pass2.SetTarget();
-		}
+	xMOV(edx, ptr32[&cpuRegs.GPR.r[_Rs_].UL[1]]);
+	xCMP(edx, ptr32[&cpuRegs.GPR.r[_Rt_].UL[1]]);
+	xForwardJump8 pass1(sign ? Jcc_Less : Jcc_Below);
+	xForwardJump8 fail(sign ? Jcc_Greater : Jcc_Above);
+	{
+		xMOV(edx, ptr32[&cpuRegs.GPR.r[_Rs_].UL[0]]);
+		xCMP(edx, ptr32[&cpuRegs.GPR.r[_Rt_].UL[0]]);
+		xForwardJump8 pass2(Jcc_Below);
+
+		fail.SetTarget();
+		xMOV(eax, 0);
+		pass2.SetTarget();
 	}
+	pass1.SetTarget();
 
 	xMOV(ptr32[&cpuRegs.GPR.r[_Rd_].UL[0]], eax);
 	xMOV(ptr32[&cpuRegs.GPR.r[_Rd_].UL[1]], 0);

commit 214429474ce7f7ccc33042549011c7d282781e39

* Fix patches, which were broken in r3398 (they weren't being applied).  * Remove some dangerous uses of pxAssume -- pxAssume should only be used for simple in-place operations, and should never call functions (my fault too, I was the one who was using it wrong).
git-svn-id: http://pcsx2.googlecode.com/svn/trunk@3410 96395faa-99c1-11dd-bbfe-3dabce05a288

common/include/Utilities/Dependencies.h
@@ -86,8 +86,6 @@ extern const wxPoint wxDefaultPosition;
 		return ((int)id >= enumName##_FIRST) && ((int)id < enumName##_COUNT); } \
 	static __forceinline bool EnumAssert( enumName id ) { \
 		return pxAssert( EnumIsValid(id) ); } \
-	static __forceinline void EnumAssume( enumName id ) { \
-		pxAssume( EnumIsValid(id) ); } \
  \
 	extern const wxChar* EnumToString( enumName id )
 

common/include/x86emitter/x86types.h
@@ -355,7 +355,6 @@ template< typename T > void xWrite( T val );
 
 	public:
 		xRegisterSSE(): _parent() {}
-		//xRegisterSSE( const xRegisterBase& src ) : _parent( src ) {}
 		explicit xRegisterSSE( int regId ) : _parent( regId ) {}
 
 		virtual uint GetOperandSize() const { return 16; }
@@ -374,14 +373,8 @@ template< typename T > void xWrite( T val );
 			--Id &= (iREGCNT_XMM-1);
 			return *this;
 		}
-		
-		static const xRegisterSSE* const m_tbl_xmmRegs[iREGCNT_XMM];
 
-		static const xRegisterSSE& GetInstance(uint id)
-		{
-			pxAssume(id < iREGCNT_XMM);
-			return *m_tbl_xmmRegs[id];
-		}
+		static const inline xRegisterSSE& GetInstance(uint id);
 	};
 
 	class xRegisterCL : public xRegister8
@@ -498,6 +491,19 @@ template< typename T > void xWrite( T val );
 
 	extern const xRegisterCL cl;		// I'm special!
 
+	const xRegisterSSE& xRegisterSSE::GetInstance(uint id)
+	{
+		static const xRegisterSSE *const m_tbl_xmmRegs[iREGCNT_XMM] = 
+		{
+			&xmm0, &xmm1, 
+			&xmm2, &xmm3, 
+			&xmm4, &xmm5,
+			&xmm6, &xmm7
+		};
+
+		pxAssume(id < iREGCNT_XMM);
+		return *m_tbl_xmmRegs[id];
+	}
 
 	// --------------------------------------------------------------------------------------
 	//  xAddressVoid

common/src/Utilities/wxAppWithHelpers.cpp
@@ -596,7 +596,7 @@ void wxAppWithHelpers::ProcessAction( pxActionEvent& evt )
 
 void wxAppWithHelpers::DeleteObject( BaseDeletableObject& obj )
 {
-	pxAssume( !obj.IsBeingDeleted() );
+	pxAssert( !obj.IsBeingDeleted() );
 	wxCommandEvent evt( pxEvt_DeleteObject );
 	evt.SetClientData( (void*)&obj );
 	AddIdleEvent( evt );

common/src/x86emitter/x86emitter.cpp
@@ -138,14 +138,6 @@ const xRegister8
 
 const xRegisterCL cl;
 
-const xRegisterSSE *const xRegisterSSE::m_tbl_xmmRegs[iREGCNT_XMM] = 
-{
-	&xmm0, &xmm1, 
-	&xmm2, &xmm3, 
-	&xmm4, &xmm5,
-	&xmm6, &xmm7
-};
-
 const char *const x86_regnames_gpr8[8] =
 {
 	"al", "cl", "dl", "bl",

pcsx2/GameDatabase.h
@@ -63,7 +63,7 @@ struct key_pair {
 
 	wxString toString() const {
 		if (key[0] == '[') {
-			pxAssumeDev( key.EndsWith(L"]"), "Malformed multiline key detected: missing end bracket!" );
+			pxAssertDev( key.EndsWith(L"]"), "Malformed multiline key detected: missing end bracket!" );
 
 			// Terminating tag must be written without the "rvalue" -- in the form of:
 			//   [/patches]

pcsx2/Pcsx2Config.cpp
@@ -292,7 +292,7 @@ void Pcsx2Config::GamefixOptions::Set( const wxString& list, bool enabled )
 
 void Pcsx2Config::GamefixOptions::Set( GamefixId id, bool enabled )
 {
-	EnumAssume( id );
+	EnumAssert( id );
 	switch(id)
 	{
 		case Fix_VuAddSub:		VuAddSubHack		= enabled;	break;
@@ -311,7 +311,7 @@ void Pcsx2Config::GamefixOptions::Set( GamefixId id, bool enabled )
 
 bool Pcsx2Config::GamefixOptions::Get( GamefixId id ) const
 {
-	EnumAssume( id );
+	EnumAssert( id );
 	switch(id)
 	{
 		case Fix_VuAddSub:		return VuAddSubHack;

pcsx2/PluginManager.cpp
@@ -1270,7 +1270,7 @@ bool SysCorePlugins::Shutdown()
 {
 	if( !NeedsShutdown() ) return false;
 
-	pxAssumeDev( !NeedsClose(), "Cannot shut down plugins prior to Close()" );
+	pxAssertDev( !NeedsClose(), "Cannot shut down plugins prior to Close()" );
 	
 	GetMTGS().Cancel();	// cancel it for speedier shutdown!
 	

pcsx2/gui/AppCoreThread.cpp
@@ -296,6 +296,7 @@ void AppCoreThread::ApplySettings( const Pcsx2Config& src )
 
 	const wxString newGameKey( SysGetDiscID() );
 	const bool verbose( newGameKey != curGameKey );
+	curGameKey = newGameKey;
 
 	if (IGameDatabase* GameDB = AppHost_GetGameDatabase() )
 	{
@@ -323,8 +324,6 @@ void AppCoreThread::ApplySettings( const Pcsx2Config& src )
 			gameCheats.Printf(L" [%d Cheats]", cheats);
 		}
 	}
-
-	curGameKey = newGameKey;
 
 	Console.SetTitle(gameName+gameSerial+gameCompat+gameFixes+gamePatch+gameCheats);
 

pcsx2/gui/AppMain.cpp
@@ -589,7 +589,7 @@ MainEmuFrame& Pcsx2App::GetMainFrame() const
 	MainEmuFrame* mainFrame = GetMainFramePtr();
 
 	pxAssume( mainFrame != NULL );
-	pxAssume( ((uptr)GetTopWindow()) == ((uptr)mainFrame) );
+	pxAssert( ((uptr)GetTopWindow()) == ((uptr)mainFrame) );
 	return *mainFrame;
 }
 
@@ -866,7 +866,7 @@ void Pcsx2App::OpenGsPanel()
 		gsFrame->SetSize( oldsize );
 	}
 	
-	pxAssumeDev( !GetCorePlugins().IsOpen( PluginId_GS ), "GS Plugin must be closed prior to opening a new Gs Panel!" );
+	pxAssertDev( !GetCorePlugins().IsOpen( PluginId_GS ), "GS Plugin must be closed prior to opening a new Gs Panel!" );
 	pDsp = (uptr)gsFrame->GetViewport()->GetHandle();
 
 	gsFrame->ShowFullScreen( g_Conf->GSWindow.IsFullscreen );