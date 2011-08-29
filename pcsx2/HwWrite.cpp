SgtBilko76/ARMSX2-Quest

commit 38c987a43a6249c75906762954b3005df9788f40

Linux/GCC fixes. :)
git-svn-id: http://pcsx2.googlecode.com/svn/trunk@3705 96395faa-99c1-11dd-bbfe-3dabce05a288

pcsx2/HwWrite.cpp
@@ -29,8 +29,9 @@ using namespace R5900;
 #define HELPSWITCH(m) (((m)>>4) & 0xff)
 #define mcase(src) case HELPSWITCH(src)
 
-template< uint page > void __fastcall hwWrite8(u32 mem, u8 value);
-template< uint page > void __fastcall hwWrite16(u32 mem, u8 value);
+template< uint page > void __fastcall _hwWrite8(u32 mem, u8 value);
+template< uint page > void __fastcall _hwWrite16(u32 mem, u8 value);
+template< uint page > void __fastcall _hwWrite128(u32 mem, u8 value);
 
 
 template<uint page>

common/include/Pcsx2Types.h
@@ -134,20 +134,25 @@ union u128
 	// Explicit conversion from u64. Zero-extends the source through 128 bits.
 	static u128 From64( u64 src )
 	{
-		u128 retval = { src, 0 };
+		u128 retval;
+		retval.lo = src;
+		retval.hi = 0;
 		return retval;
 	}
 
 	// Explicit conversion from u32. Zero-extends the source through 128 bits.
 	static u128 From32( u32 src )
 	{
-		u128 retval = { src, 0 };
+		u128 retval;
+		retval._u32[0] = src;
+		retval._u32[1] = 0;
+		retval.hi = 0;
 		return retval;
 	}
 
-	operator u32() const { return (u32)lo; }
-	operator u16() const { return (u16)lo; }
-	operator u8() const { return (u8)lo; }
+	operator u32() const { return _u32[0]; }
+	operator u16() const { return _u16[0]; }
+	operator u8() const { return _u8[0]; }
 	
 	bool operator==( const u128& right ) const
 	{

pcsx2/CMakeLists.txt
@@ -402,7 +402,7 @@ set(pcsx2ps2Sources
 set(pcsx2ps2Headers
 	ps2/BiosTools.h
 	ps2/eeHwTraceLog.inl
-	ps2/HwInternal.inl
+	ps2/HwInternal.h
 	ps2/Iop/IopHw_Internal.h)
 
 # RDebug sources

pcsx2/HwRead.cpp
@@ -34,6 +34,8 @@ static __fi void IntCHackCheck()
 static const uint HwF_VerboseConLog	= 1<<0;
 static const uint HwF_IntcStatHack	= 1<<1;	// used for Reads only.
 
+template< uint page > void __fastcall _hwRead128(u32 mem, mem128_t* result );
+
 template< uint page, bool intcstathack >
 mem32_t __fastcall _hwRead32(u32 mem)
 {
@@ -68,7 +70,7 @@ mem32_t __fastcall _hwRead32(u32 mem)
 			DevCon.WriteLn( Color_Cyan, "Reading 32-bit FIFO data" );
 
 			u128 out128;
-			hwRead128<page>(mem, &out128);
+			_hwRead128<page>(mem, &out128);
 			return out128._u32[(mem >> 2) & 0x3];
 		}
 		break;

pcsx2/ps2/Iop/IopHw_Internal.h
@@ -209,7 +209,7 @@ static __ri void IopHwTraceLog( u32 addr, T val, bool mode )
 	FastFormatAscii labelStr;
 	labelStr.Write("Hw%s%u", mode ? "Read" : "Write", sizeof (T) * 8);
 
-	switch( sizeof T )
+	switch( sizeof (T) )
 	{
 	case 1: valStr.Write("0x%02x", val); break;
 	case 2: valStr.Write("0x%04x", val); break;

pcsx2/ps2/eeHwTraceLog.inl
@@ -249,7 +249,7 @@ static __ri void eeHwTraceLog( u32 addr, T val, bool mode )
 	FastFormatAscii labelStr;
 	labelStr.Write("Hw%s%u", mode ? "Read" : "Write", sizeof (T) * 8);
 
-	switch( sizeof T )
+	switch( sizeof(T) )
 	{
 		case 1: valStr.Write("0x%02x", val); break;
 		case 2: valStr.Write("0x%04x", val); break;

commit 06941eaa2e63f3edb2dfdbe52d6dcbc2bb6af655

Fixed Virtual on Marz (or Mars) which does an 8 bit DMAC_STAT write. This broke in r3705, which handles the write instead of ignoring it.
git-svn-id: http://pcsx2.googlecode.com/svn/trunk@4882 96395faa-99c1-11dd-bbfe-3dabce05a288

pcsx2/HwWrite.cpp
@@ -239,6 +239,14 @@ void __fastcall _hwWrite8(u32 mem, u8 value)
 		}
 		return;
 	}
+	icase(DMAC_STAT) // Virtual on Marz sound effect hang. Broke in r3705 (handled in r3704 HwWrite.cpp line 352)
+	{
+		static bool warnedOnce = false;
+		if (!warnedOnce)
+			DevCon.Warning ( "8bit DMAC_STAT write, ignoring" );
+		warnedOnce = true;
+		return; // r3704 ignored 8 bit DMAC_STAT writes. Is that okay? (rama)
+	}
 
 	u32 merged = _hwRead32<page,false>(mem & ~0x03);
 	((u8*)&merged)[mem & 0x3] = value;