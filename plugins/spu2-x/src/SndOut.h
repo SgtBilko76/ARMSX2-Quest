ssakash/pcsx2

SPU2-X: New INI variable for cycle delay

plugins/spu2-x/src/SndOut.h
@@ -45,6 +45,7 @@ extern float VolumeAdjustBR;
 extern float VolumeAdjustSL;
 extern float VolumeAdjustSR;
 extern float VolumeAdjustLFE;
+extern int delay_cycles;
 
 struct Stereo51Out16DplII;
 struct Stereo51Out32DplII;

plugins/spu2-x/src/spu2sys.cpp
@@ -306,7 +306,7 @@ void V_Core::UpdateEffectsBufferSize()
 
 void V_Voice::QueueStart()
 {
-	if (Cycles - PlayCycle < 4)
+	if (Cycles - PlayCycle < delay_cycles)
 	{
 		// Required by The Legend of Spyro: The Eternal Night (probably the other two legend games too)
 		ConLog(" *** KeyOn after less than 4 T disregarded.\n");
@@ -317,7 +317,7 @@ void V_Voice::QueueStart()
 
 bool V_Voice::Start()
 {
-	if((Cycles-PlayCycle)>=4)
+	if((Cycles-PlayCycle)>= delay_cycles)
 	{
 		if(StartA&7)
 		{

SgtBilko76/ARMSX2-Quest

plugins/spu2-x/src/SndOut.h
@@ -45,6 +45,7 @@ extern float VolumeAdjustBR;
 extern float VolumeAdjustSL;
 extern float VolumeAdjustSR;
 extern float VolumeAdjustLFE;
+extern int delayCycles;
 
 struct Stereo51Out16DplII;
 struct Stereo51Out32DplII;

plugins/spu2-x/src/spu2sys.cpp
@@ -306,7 +306,7 @@ void V_Core::UpdateEffectsBufferSize()
 
 void V_Voice::QueueStart()
 {
-	if (Cycles - PlayCycle < 4)
+	if (Cycles - PlayCycle < delayCycles)
 	{
 		// Required by The Legend of Spyro: The Eternal Night (probably the other two legend games too)
 		ConLog(" *** KeyOn after less than 4 T disregarded.\n");
@@ -317,7 +317,7 @@ void V_Voice::QueueStart()
 
 bool V_Voice::Start()
 {
-	if((Cycles-PlayCycle)>=4)
+	if((Cycles-PlayCycle)>= delayCycles)
 	{
 		if(StartA&7)
 		{