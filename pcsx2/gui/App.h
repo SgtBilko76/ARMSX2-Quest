SgtBilko76/ARMSX2-Quest

commit 8b4d82d10f953c828036f09ab1823e8b2cf99df6

pcsx2:cli: support the elf option properly

pcsx2/gui/App.h
@@ -290,11 +290,14 @@ public:
 	// is set to ISO.
 	wxString		IsoFile;
 
+	wxString		ElfFile;
+
 	// Specifies the CDVD source type to use when AutoRunning
 	CDVD_SourceType CdvdSource;
 
 	// Indicates if PCSX2 should autorun the configured CDVD source and/or ISO file.
 	bool			SysAutoRun;
+	bool			SysAutoRunElf;
 
 	StartupOptions()
 	{
@@ -303,7 +306,10 @@ public:
 		PortableMode			= false;
 		NoFastBoot				= false;
 		SysAutoRun				= false;
+		SysAutoRunElf			= false;
 		CdvdSource				= CDVDsrc_NoDisc;
+		IsoFile					= "";
+		ElfFile					= "";
 	}
 };
 

pcsx2/gui/AppInit.cpp
@@ -346,6 +346,14 @@ bool Pcsx2App::OnCmdLineParsed( wxCmdLineParser& parser )
 		Startup.IsoFile		= parser.GetParam( 0 );
 		Startup.SysAutoRun	= true;
 	}
+	else
+	{
+		wxString elf_file;
+		if (parser.Found(L"elf", &elf_file) && !elf_file.IsEmpty()) {
+			Startup.SysAutoRunElf = true;
+			Startup.ElfFile = elf_file;
+		}
+	}
 	
 	if( parser.Found(L"usecd") )
 	{
@@ -484,6 +492,15 @@ bool Pcsx2App::OnInit()
 			g_Conf->EmuOptions.UseBOOT2Injection = !Startup.NoFastBoot;
 			SysUpdateIsoSrcFile( Startup.IsoFile );
 			sApp.SysExecute( Startup.CdvdSource );
+		}
+		else if ( Startup.SysAutoRunElf )
+		{
+			g_Conf->EmuOptions.UseBOOT2Injection = true;
+			// Enable iop/ee logging
+			SysConsole.eeConsole.Enabled = true;
+			SysConsole.iopConsole.Enabled = true;
+
+			sApp.SysExecute( Startup.CdvdSource, Startup.ElfFile );
 		}
 	}
 	// ----------------------------------------------------------------------------

commit 26315954b3cfc0598b35059ca367df455b606ac8

Fix build error with wxWidgets 2.8. Regression introduced in 8b4d82d10f953c828036f09ab1823e8b2cf99df6; assigning "" to a wxString results in an ambiguity error when building with wxWidgets 2.8 (this could well be a bug in wxWidgets).  The default wxString constructor creates an empty string anyway, so these assignments are unnecessary.

pcsx2/gui/App.h
@@ -308,8 +308,6 @@ public:
 		SysAutoRun				= false;
 		SysAutoRunElf			= false;
 		CdvdSource				= CDVDsrc_NoDisc;
-		IsoFile					= "";
-		ElfFile					= "";
 	}
 };
 