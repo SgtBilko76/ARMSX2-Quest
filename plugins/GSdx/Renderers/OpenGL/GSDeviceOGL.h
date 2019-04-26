Narugakuruga/pcsx2

gsdx ogl: enable sparse feature on GSdevice interface
Note: remain
* To really use sparse texture
* To debug it ^^

plugins/GSdx/Renderers/OpenGL/GSDeviceOGL.h
@@ -562,6 +562,8 @@ public:
 	void OMSetRenderTargets(GSTexture* rt, GSTexture* ds, const GSVector4i* scissor = NULL) final;
 	void OMSetColorMaskState(OMColorMaskSelector sel = OMColorMaskSelector());
 
+	virtual bool HasDepthSparse() { return GLLoader::found_compatible_GL_ARB_sparse_texture2; }
+	virtual bool HasColorSparse() { return GLLoader::found_compatible_sparse_depth; }
 
 	void CreateTextureFX();
 	GLuint CompileVS(VSSelector sel);

SgtBilko76/ARMSX2-Quest

plugins/GSdx/Renderers/OpenGL/GSDeviceOGL.h
@@ -563,6 +563,8 @@ public:
 	void OMSetRenderTargets(GSTexture* rt, GSTexture* ds, const GSVector4i* scissor = NULL) final;
 	void OMSetColorMaskState(OMColorMaskSelector sel = OMColorMaskSelector());
 
+	virtual bool HasColorSparse() { return GLLoader::found_compatible_GL_ARB_sparse_texture2; }
+	virtual bool HasDepthSparse() { return GLLoader::found_compatible_sparse_depth; }
 
 	void CreateTextureFX();
 	GLuint CompileVS(VSSelector sel);