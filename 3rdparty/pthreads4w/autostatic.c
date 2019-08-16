SgtBilko76/ARMSX2-Quest

commit 3a22ef49d0a631a745a7a09b62fa79519abeae67

3rdparty/pthreads4w: Fixup process exit logic. (#3056)
This PR modifies a third party module - I realize normally those should be fixed upstream, but I really doubt pthreads4w would a) consider it a valid bug and b) fix it. To make all my changes easily visible, I wrap them all in #if PCSX2_FIX.

This PR fixes a process exit routine in pthreads. This third party module exploits CRT initialization order to inject their initializer/deinitializer earlier than the others by putting their functions in .CRT$XCU and .CRT$XPU pseudo regions. The problem comes when a module gets build with dynamic CRT (/MD or /MDd), like most of PCSX2 plugins, it doesn't actually use .CRT$XPx regions as terminators, and instead lets dynamic CRT handle them.

This PR corrects this issue by registering the terminator via atexit, so it works with both static and dynamic CRT. This resolves an issue where SPU2-X plugin (and potentially more) leaks TLS handles when unloaded.

3rdparty/pthreads4w/autostatic.c
@@ -36,21 +36,33 @@
 
 #if defined(PTW32_STATIC_LIB)
 
+#define PCSX2_FIX 1 // Comment out to restore code to pristine state
+
 #if defined(__MINGW64__) || defined(__MINGW32__) || defined(_MSC_VER)
 
 #include "pthread.h"
 #include "implement.h"
 
+#if !PCSX2_FIX
 static void on_process_init(void)
 {
     pthread_win32_process_attach_np ();
 }
+#endif
 
 static void on_process_exit(void)
 {
     pthread_win32_thread_detach_np  ();
     pthread_win32_process_detach_np ();
 }
+
+#if PCSX2_FIX
+static void on_process_init(void)
+{
+    pthread_win32_process_attach_np();
+    atexit(on_process_exit);
+}
+#endif
 
 #if defined(__MINGW64__) || defined(__MINGW32__)
 # define attribute_section(a) __attribute__((section(a)))
@@ -62,7 +74,9 @@ attribute_section(".ctors") void *gcc_ctor = on_process_init;
 attribute_section(".dtors") void *gcc_dtor = on_process_exit;
 
 attribute_section(".CRT$XCU") void *msc_ctor = on_process_init;
+#if !PCSX2_FIX
 attribute_section(".CRT$XPU") void *msc_dtor = on_process_exit;
+#endif
 
 #endif /* defined(__MINGW64__) || defined(__MINGW32__) || defined(_MSC_VER) */
 

commit 488efabab5bb93478c70f96603b473d95b68d2b5

3rdparty/pthreads4w: Ensure on_process_init early execution. (#3073)
This fixes a semi-consistent regression introduced by #3056, where on_process_exit would execute before other destructors relying on pthreads.

Now on_process_init has been moved to an earlier section of static initializers, ensuring it will be initialized first. Previously, this initializer was placed in the same section as any other initializer, making their order of execution non-deterministic across compilations - for example, I was unable to reproduce this issue as soon as I forced pthreads4w.c to recompile last!

3rdparty/pthreads4w/autostatic.c
@@ -73,9 +73,14 @@ static void on_process_init(void)
 attribute_section(".ctors") void *gcc_ctor = on_process_init;
 attribute_section(".dtors") void *gcc_dtor = on_process_exit;
 
-attribute_section(".CRT$XCU") void *msc_ctor = on_process_init;
 #if !PCSX2_FIX
+attribute_section(".CRT$XCU") void *msc_ctor = on_process_init;
 attribute_section(".CRT$XPU") void *msc_dtor = on_process_exit;
+#else
+// MSVC puts all initializers in .CRT$XCU and order of those is not fully deterministic
+// Since the entire point of this is to call on_process_init before any other initializer,
+// move it to .CRT$XCP so it is guaranteed it initializes early
+attribute_section(".CRT$XCP") void *msc_ctor = on_process_init;
 #endif
 
 #endif /* defined(__MINGW64__) || defined(__MINGW32__) || defined(_MSC_VER) */