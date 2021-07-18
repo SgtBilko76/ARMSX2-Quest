ps2homebrew/pcsx2

cmake: Use imported targets

cmake/SearchForStuff.cmake
@@ -3,22 +3,25 @@
 #-------------------------------------------------------------------------------
 ## Use cmake package to find module
 if (Linux)
-	find_package(ALSA)
+	find_package(ALSA REQUIRED)
+	make_imported_target_if_missing(ALSA::ALSA ALSA)
 endif()
-find_package(PCAP)
-find_package(LibXml2)
-find_package(Freetype) # GSdx OSD
+find_package(PCAP REQUIRED)
+find_package(LibXml2 REQUIRED)
+make_imported_target_if_missing(LibXml2::LibXml2 LibXml2)
+find_package(Freetype REQUIRED) # GSdx OSD
 find_package(Gettext) # translation tool
 if(EXISTS ${PROJECT_SOURCE_DIR}/.git)
 	find_package(Git)
 endif()
-find_package(LibLZMA)
+find_package(LibLZMA REQUIRED)
+make_imported_target_if_missing(LibLZMA::LibLZMA LIBLZMA)
 
 # Using find_package OpenGL without either setting your opengl preference to GLVND or LEGACY
 # is deprecated as of cmake 3.11.
 set(OpenGL_GL_PREFERENCE GLVND)
-find_package(OpenGL)
-find_package(PNG)
+find_package(OpenGL REQUIRED)
+find_package(PNG REQUIRED)
 find_package(Vtune)
 
 # The requirement of wxWidgets is checked in SelectPcsx2Plugins module
@@ -87,20 +90,21 @@ else()
 	endif()
 endif()
 
-find_package(wxWidgets COMPONENTS base core adv)
-find_package(ZLIB)
+find_package(wxWidgets REQUIRED base core adv)
+include(${wxWidgets_USE_FILE})
+make_imported_target_if_missing(wxWidgets::all wxWidgets)
+
+find_package(ZLIB REQUIRED)
 
 ## Use pcsx2 package to find module
 include(FindLibc)
-
-## Use pcsx2 package to find module
-include(Findlibsamplerate)
 
 ## Use pcsx2 package to find module
 include(FindPulseAudio)
 
 ## Use CheckLib package to find module
 include(CheckLib)
+
 if(Linux)
 	check_lib(EGL EGL EGL/egl.h)
 	check_lib(X11_XCB X11-xcb X11/Xlib-xcb.h)
@@ -118,26 +122,32 @@ endif()
 if(PORTAUDIO_API)
 	check_lib(PORTAUDIO portaudio portaudio.h pa_linux_alsa.h)
 endif()
-check_lib(SOUNDTOUCH SoundTouch soundtouch/SoundTouch.h)
+check_lib(SOUNDTOUCH SoundTouch SoundTouch.h PATH_SUFFIXES soundtouch)
+check_lib(SAMPLERATE samplerate samplerate.h)
 
 if(SDL2_API)
 	check_lib(SDL2 SDL2 SDL.h PATH_SUFFIXES SDL2)
+	alias_library(SDL::SDL PkgConfig::SDL2)
 else()
 	# Tell cmake that we use SDL as a library and not as an application
 	set(SDL_BUILDING_LIBRARY TRUE)
-	find_package(SDL)
+	find_package(SDL REQUIRED)
 endif()
 
 if(UNIX)
-	find_package(X11)
+	find_package(X11 REQUIRED)
+	make_imported_target_if_missing(X11::X11 X11)
 	# Most plugins (if not all) and PCSX2 core need gtk2, so set the required flags
 	if (GTK2_API)
-	find_package(GTK2 REQUIRED gtk)
+		find_package(GTK2 REQUIRED gtk)
+		alias_library(GTK::gtk GTK2::gtk)
 	else()
 	if(CMAKE_CROSSCOMPILING)
 		find_package(GTK3 REQUIRED gtk)
+		alias_library(GTK::gtk GTK3::gtk)
 	else()
 		check_lib(GTK3 gtk+-3.0 gtk/gtk.h)
+		alias_library(GTK::gtk PkgConfig::GTK3)
 	endif()
 	endif()
 endif()
@@ -145,77 +155,7 @@ endif()
 #----------------------------------------
 #		    Use system include
 #----------------------------------------
-if(UNIX)
-	if(GTK3_FOUND)
-		include_directories(${GTK3_INCLUDE_DIRS})
-		# A lazy solution
-		set(GTK2_LIBRARIES ${GTK3_LIBRARIES})
-	elseif(GTK2_FOUND)
-		include_directories(${GTK2_INCLUDE_DIRS})
-	endif()
-
-	if(X11_FOUND)
-		include_directories(${X11_INCLUDE_DIR})
-	endif()
-endif()
-
-if(ALSA_FOUND)
-	include_directories(${ALSA_INCLUDE_DIRS})
-endif()
-
-if(CG_FOUND)
-	include_directories(${CG_INCLUDE_DIRS})
-endif()
-
-if(JPEG_FOUND)
-	include_directories(${JPEG_INCLUDE_DIR})
-endif()
-
-if(GLEW_FOUND)
-	include_directories(${GLEW_INCLUDE_DIR})
-endif()
-
-if(OPENGL_FOUND)
-	include_directories(${OPENGL_INCLUDE_DIR})
-endif()
-
-if(SDL_FOUND AND NOT SDL2_API)
-	include_directories(${SDL_INCLUDE_DIR})
-endif()
-
-if(USE_VTUNE AND VTUNE_FOUND)
-	include_directories(${VTUNE_INCLUDE_DIRS})
-endif()
-
-if(wxWidgets_FOUND)
-	include(${wxWidgets_USE_FILE})
-endif()
-
-if(PCAP_FOUND)
-	include_directories(${PCAP_INCLUDE_DIR})
-endif()
-
-if(LIBSAMPLERATE_FOUND)
-	include_directories(${LIBSAMPLERATE_INCLUDE_DIR})
-endif()
-
-if(PULSEAUDIO_FOUND)
-	include_directories(${PULSEAUDIO_INCLUDE_DIR})
-endif()
-
-if(LIBXML2_FOUND)
-	include_directories(${LIBXML2_INCLUDE_DIRS})
-endif()
-
-if(ZLIB_FOUND)
-	include_directories(${ZLIB_INCLUDE_DIRS})
-endif()
-
 find_package(HarfBuzz)
-
-if(HarfBuzz_FOUND)
-include_directories(${HarfBuzz_INCLUDE_DIRS})
-endif()
 
 set(ACTUALLY_ENABLE_TESTS ${ENABLE_TESTS})
 if(ENABLE_TESTS)
@@ -224,17 +164,6 @@ if(ENABLE_TESTS)
 		set(ACTUALLY_ENABLE_TESTS Off)
 	endif()
 endif()
-
-#----------------------------------------
-#  Use  project-wide include directories
-#----------------------------------------
-include_directories(
-	${CMAKE_SOURCE_DIR}/common/include
-	${CMAKE_SOURCE_DIR}/common/include/Utilities
-	${CMAKE_SOURCE_DIR}/common/include/x86emitter
-	# File generated by Cmake
-	${CMAKE_BINARY_DIR}/common/include
-)
 
 #----------------------------------------
 # Check correctness of the parameter

SgtBilko76/ARMSX2-Quest

cmake/SearchForStuff.cmake
@@ -3,22 +3,25 @@
 #-------------------------------------------------------------------------------
 ## Use cmake package to find module
 if (Linux)
-	find_package(ALSA)
+	find_package(ALSA REQUIRED)
+	make_imported_target_if_missing(ALSA::ALSA ALSA)
 endif()
-find_package(PCAP)
-find_package(LibXml2)
-find_package(Freetype) # GS OSD
+find_package(PCAP REQUIRED)
+find_package(LibXml2 REQUIRED)
+make_imported_target_if_missing(LibXml2::LibXml2 LibXml2)
+find_package(Freetype REQUIRED) # GS OSD
 find_package(Gettext) # translation tool
 if(EXISTS ${PROJECT_SOURCE_DIR}/.git)
 	find_package(Git)
 endif()
-find_package(LibLZMA)
+find_package(LibLZMA REQUIRED)
+make_imported_target_if_missing(LibLZMA::LibLZMA LIBLZMA)
 
 # Using find_package OpenGL without either setting your opengl preference to GLVND or LEGACY
 # is deprecated as of cmake 3.11.
 set(OpenGL_GL_PREFERENCE GLVND)
-find_package(OpenGL)
-find_package(PNG)
+find_package(OpenGL REQUIRED)
+find_package(PNG REQUIRED)
 find_package(Vtune)
 
 # Does not require the module (allow to compile non-wx plugins)
@@ -86,23 +89,25 @@ else()
 	endif()
 endif()
 
-find_package(wxWidgets COMPONENTS base core adv)
-find_package(ZLIB)
+find_package(wxWidgets REQUIRED base core adv)
+include(${wxWidgets_USE_FILE})
+make_imported_target_if_missing(wxWidgets::all wxWidgets)
+
+find_package(ZLIB REQUIRED)
 
 ## Use pcsx2 package to find module
 include(FindLibc)
-
-## Use pcsx2 package to find module
-include(Findlibsamplerate)
 
 ## Use pcsx2 package to find module
 include(FindPulseAudio)
 
 ## Use CheckLib package to find module
 include(CheckLib)
+
 if(Linux)
 	check_lib(EGL EGL EGL/egl.h)
 	check_lib(X11_XCB X11-xcb X11/Xlib-xcb.h)
+	check_lib(XCB xcb xcb/xcb.h)
 	check_lib(AIO aio libaio.h)
 	# There are two udev pkg config files - udev.pc (wrong), libudev.pc (correct)
 	# When cross compiling, pkg-config will be skipped so we have to look for
@@ -117,105 +122,40 @@ endif()
 if(PORTAUDIO_API)
 	check_lib(PORTAUDIO portaudio portaudio.h pa_linux_alsa.h)
 endif()
-check_lib(SOUNDTOUCH SoundTouch soundtouch/SoundTouch.h)
+check_lib(SOUNDTOUCH SoundTouch SoundTouch.h PATH_SUFFIXES soundtouch)
+check_lib(SAMPLERATE samplerate samplerate.h)
 
 if(SDL2_API)
 	check_lib(SDL2 SDL2 SDL.h PATH_SUFFIXES SDL2)
+	alias_library(SDL::SDL PkgConfig::SDL2)
 else()
 	# Tell cmake that we use SDL as a library and not as an application
 	set(SDL_BUILDING_LIBRARY TRUE)
-	find_package(SDL)
+	find_package(SDL REQUIRED)
 endif()
 
 if(UNIX)
-	find_package(X11)
+	find_package(X11 REQUIRED)
+	make_imported_target_if_missing(X11::X11 X11)
 	# Most plugins (if not all) and PCSX2 core need gtk2, so set the required flags
 	if (GTK2_API)
-	find_package(GTK2 REQUIRED gtk)
+		find_package(GTK2 REQUIRED gtk)
+		alias_library(GTK::gtk GTK2::gtk)
 	else()
 	if(CMAKE_CROSSCOMPILING)
 		find_package(GTK3 REQUIRED gtk)
+		alias_library(GTK::gtk GTK3::gtk)
 	else()
 		check_lib(GTK3 gtk+-3.0 gtk/gtk.h)
+		alias_library(GTK::gtk PkgConfig::GTK3)
 	endif()
 	endif()
-	find_package(XCB)
 endif()
 
 #----------------------------------------
 #           Use system include
 #----------------------------------------
-if(UNIX)
-	if(GTK3_FOUND)
-		include_directories(${GTK3_INCLUDE_DIRS})
-		# A lazy solution
-		set(GTK2_LIBRARIES ${GTK3_LIBRARIES})
-	elseif(GTK2_FOUND)
-		include_directories(${GTK2_INCLUDE_DIRS})
-	endif()
-
-	if(X11_FOUND)
-		include_directories(${X11_INCLUDE_DIR})
-	endif()
-endif()
-
-if(ALSA_FOUND)
-	include_directories(${ALSA_INCLUDE_DIRS})
-endif()
-
-if(CG_FOUND)
-	include_directories(${CG_INCLUDE_DIRS})
-endif()
-
-if(JPEG_FOUND)
-	include_directories(${JPEG_INCLUDE_DIR})
-endif()
-
-if(GLEW_FOUND)
-	include_directories(${GLEW_INCLUDE_DIR})
-endif()
-
-if(OPENGL_FOUND)
-	include_directories(${OPENGL_INCLUDE_DIR})
-endif()
-
-if(SDL_FOUND AND NOT SDL2_API)
-	include_directories(${SDL_INCLUDE_DIR})
-endif()
-
-if(USE_VTUNE AND VTUNE_FOUND)
-	include_directories(${VTUNE_INCLUDE_DIRS})
-endif()
-
-if(wxWidgets_FOUND)
-	include(${wxWidgets_USE_FILE})
-endif()
-
-if(PCAP_FOUND)
-	include_directories(${PCAP_INCLUDE_DIR})
-endif()
-
-if(LIBSAMPLERATE_FOUND)
-	include_directories(${LIBSAMPLERATE_INCLUDE_DIR})
-endif()
-
-if(PULSEAUDIO_FOUND)
-	include_directories(${PULSEAUDIO_INCLUDE_DIR})
-endif()
-
-if(LIBXML2_FOUND)
-	include_directories(${LIBXML2_INCLUDE_DIRS})
-endif()
-
-if(ZLIB_FOUND)
-	include_directories(${ZLIB_INCLUDE_DIRS})
-endif()
-
 find_package(HarfBuzz)
-
-if(HarfBuzz_FOUND)
-include_directories(${HarfBuzz_INCLUDE_DIRS})
-endif()
 
 set(ACTUALLY_ENABLE_TESTS ${ENABLE_TESTS})
 if(ENABLE_TESTS)
@@ -224,17 +164,6 @@ if(ENABLE_TESTS)
 		set(ACTUALLY_ENABLE_TESTS Off)
 	endif()
 endif()
-
-#----------------------------------------
-#  Use  project-wide include directories
-#----------------------------------------
-include_directories(
-	${CMAKE_SOURCE_DIR}/common/include
-	${CMAKE_SOURCE_DIR}/common/include/Utilities
-	${CMAKE_SOURCE_DIR}/common/include/x86emitter
-	# File generated by Cmake
-	${CMAKE_BINARY_DIR}/common/include
-)
 
 #----------------------------------------
 # Check correctness of the parameter
@@ -291,71 +220,3 @@ if(NOT USE_SYSTEM_YAML)
 endif()
 
 add_subdirectory(3rdparty/libchdr/libchdr EXCLUDE_FROM_ALL)
-
-
-
-#-------------------------------------------------------------------------------
-#                              Dependency message print
-#-------------------------------------------------------------------------------
-set(msg_dep_common_libs "check these libraries -> wxWidgets (>=3.0), aio")
-set(msg_dep_pcsx2       "check these libraries -> wxWidgets (>=3.0), gtk2, zlib (>=1.2.4), pcsx2 common libs")
-
-macro(print_dep str dep)
-	if (PACKAGE_MODE)
-		message(FATAL_ERROR "${str}:${dep}")
-	else()
-		message(STATUS "${str}:${dep}")
-	endif()
-endmacro(print_dep)
-
-#-------------------------------------------------------------------------------
-#                               Pcsx2 core & common libs
-#-------------------------------------------------------------------------------
-# Check for additional dependencies.
-# If all dependencies are available, including OS, build it
-#-------------------------------------------------------------------------------
-if (GTK2_FOUND OR GTK3_FOUND)
-	set(GTKn_FOUND TRUE)
-elseif(APPLE) # Not we have but that we don't change all if(gtkn) entries
-	set(GTKn_FOUND TRUE)
-else()
-	set(GTKn_FOUND FALSE)
-endif()
-
-if(SDL_FOUND OR SDL2_FOUND)
-	set(SDLn_FOUND TRUE)
-else()
-	set(SDLn_FOUND FALSE)
-endif()
-
-if(wxWidgets_FOUND)
-	set(common_libs TRUE)
-elseif(NOT EXISTS "${CMAKE_SOURCE_DIR}/common/src")
-	set(common_libs FALSE)
-else()
-	set(common_libs FALSE)
-	print_dep("Skip build of common libraries: missing dependencies" "${msg_dep_common_libs}")
-endif()
-
-# Common dependancy
-if(wxWidgets_FOUND AND ZLIB_FOUND AND common_libs AND NOT (Linux AND NOT AIO_FOUND))
-	if(OPENGL_FOUND AND X11_FOUND AND GTKn_FOUND AND ZLIB_FOUND AND PNG_FOUND AND FREETYPE_FOUND AND LIBLZMA_FOUND AND ((EGL_FOUND AND X11_XCB_FOUND) OR APPLE))
-		set(pcsx2_core TRUE)
-	else()
-		print_dep("Skip build of pcsx2 core: missing dependencies" "${msg_dep_pcsx2}")
-		set(pcsx2_core FALSE)
-	endif()
-elseif(NOT EXISTS "${CMAKE_SOURCE_DIR}/pcsx2")
-	set(pcsx2_core FALSE)
-else()
-	set(pcsx2_core FALSE)
-	print_dep("Skip build of pcsx2 core: missing dependencies" "${msg_dep_pcsx2}")
-endif()
-
-
-
-# Linux, BSD, use gtk2, but not OSX
-if(UNIX AND pcsx2_core AND NOT GTKn_FOUND AND NOT APPLE)
-	set(pcsx2_core FALSE)
-	print_dep("Skip build of pcsx2 core: missing dependencies" "${msg_dep_pcsx2}")
-endif()