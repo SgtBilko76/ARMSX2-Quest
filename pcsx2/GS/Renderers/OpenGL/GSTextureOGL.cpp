random634/pcsx2

GS: Enhance stats

pcsx2/GS/Renderers/OpenGL/GSTextureOGL.cpp
@@ -17,6 +17,7 @@
 #include <limits.h>
 #include "GSTextureOGL.h"
 #include "GLState.h"
+#include "GS/GSPerfMon.h"
 #include "GS/GSPng.h"
 
 #ifdef ENABLE_OGL_DEBUG_MEM_BW
@@ -468,6 +469,8 @@ bool GSTextureOGL::Map(GSMap& m, const GSVector4i* _r, int layer)
 
 	if (m_type == GSTexture::Offscreen)
 	{
+		g_perfmon.Put(GSPerfMon::Readbacks, 1);
+
 		// The fastest way will be to use a PBO to read the data asynchronously. Unfortunately GS
 		// architecture is waiting the data right now.
 

SgtBilko76/ARMSX2-Quest

pcsx2/GS/Renderers/OpenGL/GSTextureOGL.cpp
@@ -17,6 +17,7 @@
 #include <limits.h>
 #include "GSTextureOGL.h"
 #include "GLState.h"
+#include "GS/GSPerfMon.h"
 #include "GS/GSPng.h"
 
 #ifdef ENABLE_OGL_DEBUG_MEM_BW
@@ -393,6 +394,7 @@ bool GSTextureOGL::Update(const GSVector4i& r, const void* data, int pitch, int 
 #endif
 
 	GL_PUSH("Upload Texture %d", m_texture_id);
+	g_perfmon.Put(GSPerfMon::TextureUploads, 1);
 
 	// The easy solution without PBO
 #if 0
@@ -449,6 +451,7 @@ bool GSTextureOGL::Map(GSMap& m, const GSVector4i* _r, int layer)
 	if (m_type == Type::Texture || m_type == Type::RenderTarget)
 	{
 		GL_PUSH_("Upload Texture %d", m_texture_id); // POP is in Unmap
+		g_perfmon.Put(GSPerfMon::TextureUploads, 1);
 
 		m_clean = false;
 