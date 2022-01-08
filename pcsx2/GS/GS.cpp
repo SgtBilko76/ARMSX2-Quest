random634/pcsx2

GS: Add Vulkan renderer

pcsx2/GS/GS.cpp
@@ -21,6 +21,8 @@
 #include "Renderers/Null/GSDeviceNull.h"
 #include "Renderers/OpenGL/GSDeviceOGL.h"
 #include "Renderers/OpenGL/GSRendererOGL.h"
+#include "Renderers/Vulkan/GSDeviceVK.h"
+#include "Renderers/Vulkan/GSRendererVK.h"
 #include "GSLzma.h"
 
 #include "common/pxStreams.h"
@@ -134,6 +136,9 @@ static HostDisplay::RenderAPI GetAPIForRenderer(GSRendererType renderer)
 #endif
 			return HostDisplay::RenderAPI::OpenGL;
 
+		case GSRendererType::VK:
+			return HostDisplay::RenderAPI::Vulkan;
+
 #ifdef _WIN32
 		case GSRendererType::DX11:
 		case GSRendererType::SW:
@@ -163,6 +168,10 @@ static bool DoGSOpen(GSRendererType renderer, u8* basemem)
 		case HostDisplay::RenderAPI::OpenGL:
 		case HostDisplay::RenderAPI::OpenGLES:
 			dev = std::make_unique<GSDeviceOGL>();
+			break;
+
+		case HostDisplay::RenderAPI::Vulkan:
+			dev = std::make_unique<GSDeviceVK>();
 			break;
 
 		case HostDisplay::RenderAPI::None:
@@ -196,6 +205,10 @@ static bool DoGSOpen(GSRendererType renderer, u8* basemem)
 				case HostDisplay::RenderAPI::OpenGL:
 				case HostDisplay::RenderAPI::OpenGLES:
 					s_gs = std::make_unique<GSRendererOGL>(std::move(dev));
+					break;
+
+				case HostDisplay::RenderAPI::Vulkan:
+					s_gs = std::make_unique<GSRendererVK>(std::move(dev));
 					break;
 
 				case HostDisplay::RenderAPI::None:
@@ -1081,12 +1094,10 @@ void GSApp::Init()
 
 #ifdef _WIN32
 	m_gs_renderers.push_back(GSSetting(static_cast<uint32>(GSRendererType::DX11), "Direct3D 11", ""));
-	m_gs_renderers.push_back(GSSetting(static_cast<uint32>(GSRendererType::OGL), "OpenGL", ""));
-	m_gs_renderers.push_back(GSSetting(static_cast<uint32>(GSRendererType::SW), "Software", ""));
-#else // Linux
-	m_gs_renderers.push_back(GSSetting(static_cast<uint32>(GSRendererType::OGL), "OpenGL", ""));
-	m_gs_renderers.push_back(GSSetting(static_cast<uint32>(GSRendererType::SW), "Software", ""));
 #endif
+	m_gs_renderers.push_back(GSSetting(static_cast<uint32>(GSRendererType::OGL), "OpenGL", ""));
+	m_gs_renderers.push_back(GSSetting(static_cast<uint32>(GSRendererType::VK), "Vulkan", ""));
+	m_gs_renderers.push_back(GSSetting(static_cast<uint32>(GSRendererType::SW), "Software", ""));
 
 	// The null renderer goes third, it has use for benchmarking purposes in a release build
 	m_gs_renderers.push_back(GSSetting(static_cast<uint32>(GSRendererType::Null), "Null", ""));

SgtBilko76/ARMSX2-Quest

pcsx2/GS/GS.cpp
@@ -41,6 +41,10 @@
 #include "pcsx2/HostSettings.h"
 #endif
 
+#ifdef ENABLE_VULKAN
+#include "Renderers/Vulkan/GSDeviceVK.h"
+#endif
+
 #ifdef _WIN32
 
 #include "Renderers/DX11/GSDevice11.h"
@@ -154,6 +158,9 @@ static HostDisplay::RenderAPI GetAPIForRenderer(GSRendererType renderer)
 #endif
 			return HostDisplay::RenderAPI::OpenGL;
 
+		case GSRendererType::VK:
+			return HostDisplay::RenderAPI::Vulkan;
+
 #ifdef _WIN32
 		case GSRendererType::DX11:
 		case GSRendererType::SW:
@@ -182,6 +189,12 @@ static bool DoGSOpen(GSRendererType renderer, u8* basemem)
 		case HostDisplay::RenderAPI::OpenGLES:
 			g_gs_device = std::make_unique<GSDeviceOGL>();
 			break;
+
+#ifdef ENABLE_VULKAN
+		case HostDisplay::RenderAPI::Vulkan:
+			g_gs_device = std::make_unique<GSDeviceVK>();
+			break;
+#endif
 
 		default:
 			Console.Error("Unknown render API %u", static_cast<unsigned>(display->GetRenderAPI()));
@@ -1123,6 +1136,9 @@ void GSApp::Init()
 	m_gs_renderers.push_back(GSSetting(static_cast<u32>(GSRendererType::DX11), "Direct3D 11", ""));
 #endif
 	m_gs_renderers.push_back(GSSetting(static_cast<u32>(GSRendererType::OGL), "OpenGL", ""));
+#ifdef ENABLE_VULKAN
+	m_gs_renderers.push_back(GSSetting(static_cast<u32>(GSRendererType::VK), "Vulkan", ""));
+#endif
 	m_gs_renderers.push_back(GSSetting(static_cast<u32>(GSRendererType::SW), "Software", ""));
 
 	// The null renderer goes last, it has use for benchmarking purposes in a release build
@@ -1216,9 +1232,12 @@ void GSApp::Init()
 
 	// clang-format off
 	// Avoid to clutter the ini file with useless options
+#if defined(ENABLE_VULKAN) || defined(_WIN32)
+	m_default_configuration["Adapter"]																		= "";
+#endif
+
 #ifdef _WIN32
 	// Per OS option.
-	m_default_configuration["Adapter"]																		= "";
 	m_default_configuration["CaptureFileName"]                            = "";
 	m_default_configuration["CaptureVideoCodecDisplayName"]               = "";
 	m_default_configuration["dx_break_on_severity"]                       = "0";