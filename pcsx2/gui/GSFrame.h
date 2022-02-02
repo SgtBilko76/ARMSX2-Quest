random634/pcsx2

GS: Track DPI change events

pcsx2/gui/GSFrame.h
@@ -64,6 +64,7 @@ protected:
 
 	void OnCloseWindow( wxCloseEvent& evt );
 	void OnResize(wxSizeEvent& event);
+	void OnDPIChange(wxDPIChangedEvent& event);
 	void OnMouseEvent( wxMouseEvent& evt );
 	void OnHideMouseTimeout( wxTimerEvent& evt );
 	void OnKeyDownOrUp( wxKeyEvent& evt );

SgtBilko76/ARMSX2-Quest

pcsx2/gui/GSFrame.h
@@ -61,7 +61,7 @@ public:
 protected:
 	void AppStatusEvent_OnSettingsApplied();
 
-	void OnResize(wxSizeEvent& event);
+	void OnResize(wxEvent& event);
 	void OnMouseEvent( wxMouseEvent& evt );
 	void OnHideMouseTimeout( wxTimerEvent& evt );
 	void OnKeyDownOrUp( wxKeyEvent& evt );