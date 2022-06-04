SgtBilko76/ARMSX2-Quest

commit 8ff3606c1249aa6570352dce7ed11fe215af3ef2

Qt: Calculate icon theme from palette instead of name

pcsx2-qt/MainWindow.cpp
@@ -88,10 +88,10 @@ MainWindow::~MainWindow()
 
 void MainWindow::initialize()
 {
-	setIconThemeFromSettings();
+	setStyleFromSettings();
+	setIconThemeFromStyle();
 	m_ui.setupUi(this);
 	setupAdditionalUi();
-	setStyleFromSettings();
 	connectSignals();
 	connectVMThreadSignals(g_emu_thread);
 
@@ -549,17 +549,11 @@ void MainWindow::setStyleFromSettings()
 	}
 }
 
-void MainWindow::setIconThemeFromSettings()
+void MainWindow::setIconThemeFromStyle()
 {
-	const std::string theme(Host::GetBaseStringSettingValue("UI", "Theme", DEFAULT_THEME_NAME));
-	QString icon_theme;
-
-	if (theme == "darkfusion" || theme == "darkfusionblue" || theme == "dualtoneOrangeBlue" || theme == "ScarletDevilRed")
-		icon_theme = QStringLiteral("white");
-	else
-		icon_theme = QStringLiteral("black");
-
-	QIcon::setThemeName(icon_theme);
+	QPalette palette = qApp->palette();
+	bool dark = palette.windowText().color().value() > palette.window().color().value();
+	QIcon::setThemeName(dark ? QStringLiteral("white") : QStringLiteral("black"));
 }
 
 void MainWindow::onScreenshotActionTriggered()
@@ -1219,7 +1213,7 @@ void MainWindow::onToolsOpenDataDirectoryTriggered()
 void MainWindow::onThemeChanged()
 {
 	setStyleFromSettings();
-	setIconThemeFromSettings();
+	setIconThemeFromStyle();
 	recreate();
 }
 

pcsx2-qt/MainWindow.h
@@ -169,7 +169,7 @@ private:
 	void setupAdditionalUi();
 	void connectSignals();
 	void setStyleFromSettings();
-	void setIconThemeFromSettings();
+	void setIconThemeFromStyle();
 
 	void saveStateToConfig();
 	void restoreStateFromConfig();

commit 0bd4c69f68d7e27b01284588419f455f1bbda31f

Qt: Force game list column resize on window show
Fixes a regression from 8ff3606c1249aa6570352dce7ed11fe215af3ef2 where
the game list columns would always be too short on open.

pcsx2-qt/GameList/GameListWidget.h
@@ -52,6 +52,7 @@ public:
 	__fi GameListModel* getModel() const { return m_model; }
 
 	void initialize();
+	void resizeTableViewColumnsToFit();
 
 	void refresh(bool invalidate_cache);
 	void cancelRefresh();
@@ -97,7 +98,6 @@ protected:
 	void resizeEvent(QResizeEvent* event);
 
 private:
-	void resizeTableViewColumnsToFit();
 	void loadTableViewColumnVisibilitySettings();
 	void saveTableViewColumnVisibilitySettings();
 	void saveTableViewColumnVisibilitySettings(int column);

pcsx2-qt/MainWindow.cpp
@@ -1400,6 +1400,17 @@ void MainWindow::onPerformanceMetricsUpdated(const QString& fps_stat, const QStr
 	m_status_gs_widget->setText(gs_stat);
 }
 
+void MainWindow::showEvent(QShowEvent* event)
+{
+	QMainWindow::showEvent(event);
+
+	// This is a bit silly, but for some reason resizing *before* the window is shown
+	// gives the incorrect sizes for columns, if you set the style before setting up
+	// the rest of the window... so, instead, let's just force it to be resized on show.
+	if (isShowingGameList())
+		m_game_list_widget->resizeTableViewColumnsToFit();
+}
+
 void MainWindow::closeEvent(QCloseEvent* event)
 {
 	if (!requestShutdown(true, true, true))

pcsx2-qt/MainWindow.h
@@ -156,6 +156,7 @@ private Q_SLOTS:
 	void recreate();
 
 protected:
+	void showEvent(QShowEvent* event) override;
 	void closeEvent(QCloseEvent* event) override;
 	void dragEnterEvent(QDragEnterEvent* event) override;
 	void dropEvent(QDropEvent* event) override;