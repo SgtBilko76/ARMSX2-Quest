SgtBilko76/ARMSX2-Quest

commit 3197c95200551e880217f1e04ce694eddee5000e

Qt: Add disc path option for ELF game list entries

pcsx2-qt/Settings/GameSummaryWidget.cpp
@@ -22,7 +22,14 @@
 
 #include "GameSummaryWidget.h"
 #include "SettingsDialog.h"
+#include "MainWindow.h"
 #include "QtHost.h"
+#include "QtUtils.h"
+
+#include <QtCore/QDir>
+#include <QtWidgets/QFileDialog>
+
+#include "fmt/format.h"
 
 GameSummaryWidget::GameSummaryWidget(const GameList::Entry* entry, SettingsDialog* dialog, QWidget* parent)
 	: m_dialog(dialog)
@@ -32,34 +39,39 @@ GameSummaryWidget::GameSummaryWidget(const GameList::Entry* entry, SettingsDialo
 	const QString base_path(QtHost::GetResourcesBasePath());
 	for (int i = 0; i < m_ui.region->count(); i++)
 	{
-		m_ui.region->setItemIcon(i, QIcon(
-										QStringLiteral("%1/icons/flags/%2.png").arg(base_path).arg(GameList::RegionToString(static_cast<GameList::Region>(i)))));
+		m_ui.region->setItemIcon(i,
+			QIcon(QStringLiteral("%1/icons/flags/%2.png").arg(base_path).arg(GameList::RegionToString(static_cast<GameList::Region>(i)))));
 	}
 	for (int i = 1; i < m_ui.compatibility->count(); i++)
 	{
-		m_ui.compatibility->setItemIcon(i, QIcon(
-											   QStringLiteral("%1/icons/star-%2.png").arg(base_path).arg(i)));
+		m_ui.compatibility->setItemIcon(i, QIcon(QStringLiteral("%1/icons/star-%2.png").arg(base_path).arg(i)));
 	}
 
-	populateUi(entry);
+	m_entry_path = entry->path;
+	populateInputProfiles();
+	populateDetails(entry);
+	populateDiscPath(entry);
 
 	connect(m_ui.inputProfile, &QComboBox::currentIndexChanged, this, &GameSummaryWidget::onInputProfileChanged);
 }
 
 GameSummaryWidget::~GameSummaryWidget() = default;
 
-void GameSummaryWidget::populateUi(const GameList::Entry* entry)
+void GameSummaryWidget::populateInputProfiles()
+{
+	for (const std::string& name : PAD::GetInputProfileNames())
+		m_ui.inputProfile->addItem(QString::fromStdString(name));
+}
+
+void GameSummaryWidget::populateDetails(const GameList::Entry* entry)
 {
 	m_ui.title->setText(QString::fromStdString(entry->title));
 	m_ui.path->setText(QString::fromStdString(entry->path));
 	m_ui.serial->setText(QString::fromStdString(entry->serial));
-	m_ui.crc->setText(QString::fromStdString(StringUtil::StdStringFromFormat("%08X", entry->crc)));
+	m_ui.crc->setText(QString::fromStdString(fmt::format("{:08X}", entry->crc)));
 	m_ui.type->setCurrentIndex(static_cast<int>(entry->type));
 	m_ui.region->setCurrentIndex(static_cast<int>(entry->region));
 	m_ui.compatibility->setCurrentIndex(static_cast<int>(entry->compatibility_rating));
-
-	for (const std::string& name : PAD::GetInputProfileNames())
-		m_ui.inputProfile->addItem(QString::fromStdString(name));
 
 	std::optional<std::string> profile(m_dialog->getStringValue("EmuCore", "InputProfileName", std::nullopt));
 	if (profile.has_value())
@@ -68,10 +80,60 @@ void GameSummaryWidget::populateUi(const GameList::Entry* entry)
 		m_ui.inputProfile->setCurrentIndex(0);
 }
 
+void GameSummaryWidget::populateDiscPath(const GameList::Entry* entry)
+{
+	if (entry->type == GameList::EntryType::ELF)
+	{
+		std::optional<std::string> iso_path(m_dialog->getStringValue("EmuCore", "DiscPath", std::nullopt));
+		if (!iso_path->empty())
+			m_ui.discPath->setText(QString::fromStdString(iso_path.value()));
+
+		connect(m_ui.discPath, &QLineEdit::textChanged, this, &GameSummaryWidget::onDiscPathChanged);
+		connect(m_ui.discPathBrowse, &QPushButton::clicked, this, &GameSummaryWidget::onDiscPathBrowseClicked);
+		connect(m_ui.discPathClear, &QPushButton::clicked, m_ui.discPath, &QLineEdit::clear);
+	}
+	else
+	{
+		// Makes no sense to have disc override for a disc.
+		m_ui.detailsFormLayout->removeRow(8);
+		m_ui.discPath = nullptr;
+		m_ui.discPathBrowse = nullptr;
+		m_ui.discPathClear = nullptr;
+	}
+}
+
 void GameSummaryWidget::onInputProfileChanged(int index)
 {
 	if (index == 0)
 		m_dialog->setStringSettingValue("EmuCore", "InputProfileName", std::nullopt);
 	else
 		m_dialog->setStringSettingValue("EmuCore", "InputProfileName", m_ui.inputProfile->itemText(index).toUtf8());
+}
+
+void GameSummaryWidget::onDiscPathChanged(const QString& value)
+{
+	if (value.isEmpty())
+		m_dialog->removeSettingValue("EmuCore", "DiscPath");
+	else
+		m_dialog->setStringSettingValue("EmuCore", "DiscPath", value.toStdString().c_str());
+
+	// force rescan of elf to update the serial
+	g_main_window->rescanFile(m_entry_path);
+
+	// and re-fill our details (mainly the serial)
+	auto lock = GameList::GetLock();
+	const GameList::Entry* entry = GameList::GetEntryForPath(m_entry_path.c_str());
+	if (entry)
+		populateDetails(entry);
+}
+
+void GameSummaryWidget::onDiscPathBrowseClicked()
+{
+	const QString filename(QFileDialog::getOpenFileName(
+		QtUtils::GetRootWidget(this), tr("Select Disc Path"), QString(), qApp->translate("MainWindow", MainWindow::DISC_IMAGE_FILTER)));
+	if (filename.isEmpty())
+		return;
+
+	// let the signal take care of it
+	m_ui.discPath->setText(QDir::toNativeSeparators(filename));
 }

pcsx2-qt/GameList/GameListWidget.cpp
@@ -35,6 +35,8 @@
 #include "QtHost.h"
 #include "QtUtils.h"
 
+#include "fmt/format.h"
+
 static const char* SUPPORTED_FORMATS_STRING = QT_TRANSLATE_NOOP(GameListWidget,
 	".bin/.iso (ISO Disc Images)\n"
 	".chd (Compressed Hunks of Data)\n"
@@ -594,6 +596,19 @@ const GameList::Entry* GameListWidget::getSelectedEntry() const
 
 		return GameList::GetEntryByIndex(source_index.row());
 	}
+}
+
+void GameListWidget::rescanFile(const std::string& path)
+{
+	// We can't do this while there's a VM running, because of CDVD state... ugh.
+	if (QtHost::IsVMValid())
+	{
+		Console.Error(fmt::format("Can't re-scan ELF at '{}' because we have a VM running.", path));
+		return;
+	}
+
+	GameList::RescanPath(path);
+	m_model->refresh();
 }
 
 GameListGridListView::GameListGridListView(QWidget* parent /*= nullptr*/)

pcsx2-qt/GameList/GameListWidget.h
@@ -64,6 +64,9 @@ public:
 
 	const GameList::Entry* getSelectedEntry() const;
 
+	/// Rescans a single file. NOTE: Happens on UI thread.
+	void rescanFile(const std::string& path);
+
 Q_SIGNALS:
 	void refreshProgress(const QString& status, int current, int total);
 	void refreshComplete();

pcsx2-qt/MainWindow.h
@@ -78,6 +78,12 @@ public:
 	/// Default theme name for the platform.
 	static const char* DEFAULT_THEME_NAME;
 
+	/// Default filter for opening a file.
+	static const char* OPEN_FILE_FILTER;
+
+	/// Default filter for opening a disc image.
+	static const char* DISC_IMAGE_FILTER;
+
 public:
 	MainWindow();
 	~MainWindow();
@@ -100,6 +106,9 @@ public:
 	__fi QLabel* getStatusFPSWidget() const { return m_status_fps_widget; }
 	__fi QLabel* getStatusVPSWidget() const { return m_status_vps_widget; }
 
+	/// Rescans a single file. NOTE: Happens on UI thread.
+	void rescanFile(const std::string& path);
+
 public Q_SLOTS:
 	void checkForUpdates(bool display_message);
 	void refreshGameList(bool invalidate_cache);
@@ -108,7 +117,8 @@ public Q_SLOTS:
 	void reportError(const QString& title, const QString& message);
 	bool confirmMessage(const QString& title, const QString& message);
 	void runOnUIThread(const std::function<void()>& func);
-	bool requestShutdown(bool allow_confirm = true, bool allow_save_to_state = true, bool default_save_to_state = true, bool block_until_done = false);
+	bool requestShutdown(
+		bool allow_confirm = true, bool allow_save_to_state = true, bool default_save_to_state = true, bool block_until_done = false);
 	void requestExit();
 	void checkForSettingChanges();
 	std::optional<WindowInfo> getWindowInfo();
@@ -172,7 +182,7 @@ private Q_SLOTS:
 	void onVMResumed();
 	void onVMStopped();
 
-	void onGameChanged(const QString& path, const QString& serial, const QString& name, quint32 crc);
+	void onGameChanged(const QString& path, const QString& elf_override, const QString& serial, const QString& name, quint32 crc);
 
 protected:
 	void showEvent(QShowEvent* event) override;
@@ -234,8 +244,8 @@ private:
 
 	QString getDiscDevicePath(const QString& title);
 
-	void startGameListEntry(const GameList::Entry* entry, std::optional<s32> save_slot = std::nullopt,
-		std::optional<bool> fast_boot = std::nullopt);
+	void startGameListEntry(
+		const GameList::Entry* entry, std::optional<s32> save_slot = std::nullopt, std::optional<bool> fast_boot = std::nullopt);
 	void setGameListEntryCoverImage(const GameList::Entry* entry);
 	void clearGameListEntryPlayTime(const GameList::Entry* entry);
 
@@ -267,6 +277,7 @@ private:
 	QLabel* m_status_resolution_widget = nullptr;
 
 	QString m_current_disc_path;
+	QString m_current_elf_override;
 	QString m_current_game_serial;
 	QString m_current_game_name;
 	quint32 m_current_game_crc;

pcsx2-qt/QtHost.cpp
@@ -74,15 +74,16 @@ EmuThread* g_emu_thread = nullptr;
 //////////////////////////////////////////////////////////////////////////
 // Local function declarations
 //////////////////////////////////////////////////////////////////////////
-namespace QtHost {
-static void PrintCommandLineVersion();
-static void PrintCommandLineHelp(const std::string_view& progname);
-static std::shared_ptr<VMBootParameters>& AutoBoot(std::shared_ptr<VMBootParameters>& autoboot);
-static bool ParseCommandLineOptions(const QStringList& args, std::shared_ptr<VMBootParameters>& autoboot);
-static bool InitializeConfig();
-static void SaveSettings();
-static void HookSignals();
-}
+namespace QtHost
+{
+	static void PrintCommandLineVersion();
+	static void PrintCommandLineHelp(const std::string_view& progname);
+	static std::shared_ptr<VMBootParameters>& AutoBoot(std::shared_ptr<VMBootParameters>& autoboot);
+	static bool ParseCommandLineOptions(const QStringList& args, std::shared_ptr<VMBootParameters>& autoboot);
+	static bool InitializeConfig();
+	static void SaveSettings();
+	static void HookSignals();
+} // namespace QtHost
 
 //////////////////////////////////////////////////////////////////////////
 // Local variable declarations
@@ -151,8 +152,8 @@ bool EmuThread::confirmMessage(const QString& title, const QString& message)
 	{
 		// This is definitely deadlock risky, but unlikely to happen (why would GS be confirming?).
 		bool result = false;
-		QMetaObject::invokeMethod(g_emu_thread, "confirmMessage", Qt::BlockingQueuedConnection,
-			Q_RETURN_ARG(bool, result), Q_ARG(const QString&, title), Q_ARG(const QString&, message));
+		QMetaObject::invokeMethod(g_emu_thread, "confirmMessage", Qt::BlockingQueuedConnection, Q_RETURN_ARG(bool, result),
+			Q_ARG(const QString&, title), Q_ARG(const QString&, message));
 		return result;
 	}
 
@@ -231,8 +232,7 @@ void EmuThread::startVM(std::shared_ptr<VMBootParameters> boot_params)
 {
 	if (!isOnEmuThread())
 	{
-		QMetaObject::invokeMethod(this, "startVM", Qt::QueuedConnection,
-			Q_ARG(std::shared_ptr<VMBootParameters>, boot_params));
+		QMetaObject::invokeMethod(this, "startVM", Qt::QueuedConnection, Q_ARG(std::shared_ptr<VMBootParameters>, boot_params));
 		return;
 	}
 
@@ -460,9 +460,8 @@ void EmuThread::startBackgroundControllerPollTimer()
 	if (m_background_controller_polling_timer->isActive())
 		return;
 
-	m_background_controller_polling_timer->start(FullscreenUI::IsInitialized() ?
-													 FULLSCREEN_UI_CONTROLLER_POLLING_INTERVAL :
-                                                     BACKGROUND_CONTROLLER_POLLING_INTERVAL);
+	m_background_controller_polling_timer->start(
+		FullscreenUI::IsInitialized() ? FULLSCREEN_UI_CONTROLLER_POLLING_INTERVAL : BACKGROUND_CONTROLLER_POLLING_INTERVAL);
 }
 
 void EmuThread::stopBackgroundControllerPollTimer()
@@ -845,9 +844,7 @@ void EmuThread::queueSnapshot(quint32 gsdump_frames)
 	if (!VMManager::HasValidVM())
 		return;
 
-	GetMTGS().RunOnGSThread([gsdump_frames]() {
-		GSQueueSnapshot(std::string(), gsdump_frames);
-	});
+	GetMTGS().RunOnGSThread([gsdump_frames]() { GSQueueSnapshot(std::string(), gsdump_frames); });
 }
 
 void EmuThread::updateDisplay()
@@ -1014,10 +1011,12 @@ void Host::OnVMResumed()
 	emit g_emu_thread->onVMResumed();
 }
 
-void Host::OnGameChanged(const std::string& disc_path, const std::string& game_serial, const std::string& game_name, u32 game_crc)
+void Host::OnGameChanged(const std::string& disc_path, const std::string& elf_override, const std::string& game_serial,
+	const std::string& game_name, u32 game_crc)
 {
-	CommonHost::OnGameChanged(disc_path, game_serial, game_name, game_crc);
-	emit g_emu_thread->onGameChanged(QString::fromStdString(disc_path), QString::fromStdString(game_serial), QString::fromStdString(game_name), game_crc);
+	CommonHost::OnGameChanged(disc_path, elf_override, game_serial, game_name, game_crc);
+	emit g_emu_thread->onGameChanged(QString::fromStdString(disc_path), QString::fromStdString(elf_override),
+		QString::fromStdString(game_serial), QString::fromStdString(game_name), game_crc);
 }
 
 void EmuThread::updatePerformanceMetrics(bool force)
@@ -1030,12 +1029,11 @@ void EmuThread::updatePerformanceMetrics(bool force)
 		QString gs_stat;
 		if (THREAD_VU1)
 		{
-			gs_stat =
-				QStringLiteral("%1 | EE: %2% | VU: %3% | GS: %4%")
-					.arg(gs_stat_str.c_str())
-					.arg(PerformanceMetrics::GetCPUThreadUsage(), 0, 'f', 0)
-					.arg(PerformanceMetrics::GetVUThreadUsage(), 0, 'f', 0)
-					.arg(PerformanceMetrics::GetGSThreadUsage(), 0, 'f', 0);
+			gs_stat = QStringLiteral("%1 | EE: %2% | VU: %3% | GS: %4%")
+						  .arg(gs_stat_str.c_str())
+						  .arg(PerformanceMetrics::GetCPUThreadUsage(), 0, 'f', 0)
+						  .arg(PerformanceMetrics::GetVUThreadUsage(), 0, 'f', 0)
+						  .arg(PerformanceMetrics::GetGSThreadUsage(), 0, 'f', 0);
 		}
 		else
 		{
@@ -1055,17 +1053,18 @@ void EmuThread::updatePerformanceMetrics(bool force)
 	int iwidth, iheight;
 	GSgetInternalResolution(&iwidth, &iheight);
 
-	if (iwidth != m_last_internal_width || iheight != m_last_internal_height ||
-		speed != m_last_speed || gfps != m_last_game_fps || vfps != m_last_video_fps ||
-		renderer != m_last_renderer || force)
+	if (iwidth != m_last_internal_width || iheight != m_last_internal_height || speed != m_last_speed || gfps != m_last_game_fps ||
+		vfps != m_last_video_fps || renderer != m_last_renderer || force)
 	{
 		if (iwidth == 0 && iheight == 0)
 		{
 			// if we don't have width/height yet, we're not going to have fps either.
 			// and we'll probably be <100% due to compiling. so just leave it blank for now.
 			QString blank;
-			QMetaObject::invokeMethod(g_main_window->getStatusRendererWidget(), "setText", Qt::QueuedConnection, Q_ARG(const QString&, blank));
-			QMetaObject::invokeMethod(g_main_window->getStatusResolutionWidget(), "setText", Qt::QueuedConnection, Q_ARG(const QString&, blank));
+			QMetaObject::invokeMethod(
+				g_main_window->getStatusRendererWidget(), "setText", Qt::QueuedConnection, Q_ARG(const QString&, blank));
+			QMetaObject::invokeMethod(
+				g_main_window->getStatusResolutionWidget(), "setText", Qt::QueuedConnection, Q_ARG(const QString&, blank));
 			QMetaObject::invokeMethod(g_main_window->getStatusFPSWidget(), "setText", Qt::QueuedConnection, Q_ARG(const QString&, blank));
 			QMetaObject::invokeMethod(g_main_window->getStatusVPSWidget(), "setText", Qt::QueuedConnection, Q_ARG(const QString&, blank));
 			return;
@@ -1081,9 +1080,7 @@ void EmuThread::updatePerformanceMetrics(bool force)
 			if (iwidth != m_last_internal_width || iheight != m_last_internal_height || force)
 			{
 				QMetaObject::invokeMethod(g_main_window->getStatusResolutionWidget(), "setText", Qt::QueuedConnection,
-					Q_ARG(const QString&, tr("%1x%2")
-											  .arg(iwidth)
-											  .arg(iheight)));
+					Q_ARG(const QString&, tr("%1x%2").arg(iwidth).arg(iheight)));
 				m_last_internal_width = iwidth;
 				m_last_internal_height = iheight;
 			}
@@ -1091,17 +1088,14 @@ void EmuThread::updatePerformanceMetrics(bool force)
 			if (gfps != m_last_game_fps || force)
 			{
 				QMetaObject::invokeMethod(g_main_window->getStatusFPSWidget(), "setText", Qt::QueuedConnection,
-					Q_ARG(const QString&, tr("Game: %1 FPS")
-											  .arg(gfps, 0, 'f', 0)));
+					Q_ARG(const QString&, tr("Game: %1 FPS").arg(gfps, 0, 'f', 0)));
 				m_last_game_fps = gfps;
 			}
 
 			if (speed != m_last_speed || vfps != m_last_video_fps || force)
 			{
 				QMetaObject::invokeMethod(g_main_window->getStatusVPSWidget(), "setText", Qt::QueuedConnection,
-					Q_ARG(const QString&, tr("Video: %1 FPS (%2%)")
-											  .arg(vfps, 0, 'f', 0)
-											  .arg(speed, 0, 'f', 0)));
+					Q_ARG(const QString&, tr("Video: %1 FPS (%2%)").arg(vfps, 0, 'f', 0).arg(speed, 0, 'f', 0)));
 				m_last_speed = speed;
 				m_last_video_fps = vfps;
 			}
@@ -1144,10 +1138,9 @@ void Host::OnAchievementsRefreshed()
 		achievement_count = Achievements::GetAchievementCount();
 		max_points = Achievements::GetMaximumPointsForGame();
 
-		game_info = qApp->translate("EmuThread",
-							"Game ID: %1\n"
-							"Game Title: %2\n"
-							"Achievements: %5 (%6)\n\n")
+		game_info = qApp->translate("EmuThread", "Game ID: %1\n"
+												 "Game Title: %2\n"
+												 "Achievements: %5 (%6)\n\n")
 						.arg(game_id)
 						.arg(QString::fromStdString(Achievements::GetGameTitle()))
 						.arg(achievement_count)
@@ -1183,15 +1176,13 @@ void Host::RunOnCPUThread(std::function<void()> function, bool block /* = false 
 		return;
 	}
 
-	QMetaObject::invokeMethod(g_emu_thread, "runOnCPUThread",
-		block ? Qt::BlockingQueuedConnection : Qt::QueuedConnection,
+	QMetaObject::invokeMethod(g_emu_thread, "runOnCPUThread", block ? Qt::BlockingQueuedConnection : Qt::QueuedConnection,
 		Q_ARG(const std::function<void()>&, std::move(function)));
 }
 
 void Host::RefreshGameListAsync(bool invalidate_cache)
 {
-	QMetaObject::invokeMethod(g_main_window, "refreshGameList", Qt::QueuedConnection,
-		Q_ARG(bool, invalidate_cache));
+	QMetaObject::invokeMethod(g_main_window, "refreshGameList", Qt::QueuedConnection, Q_ARG(bool, invalidate_cache));
 }
 
 void Host::CancelGameListRefresh()
@@ -1213,9 +1204,8 @@ void Host::RequestVMShutdown(bool allow_confirm, bool allow_save_state, bool def
 		return;
 
 	// Run it on the host thread, that way we get the confirm prompt (if enabled).
-	QMetaObject::invokeMethod(g_main_window, "requestShutdown", Qt::QueuedConnection,
-		Q_ARG(bool, allow_confirm), Q_ARG(bool, allow_save_state),
-		Q_ARG(bool, default_save_state), Q_ARG(bool, false));
+	QMetaObject::invokeMethod(g_main_window, "requestShutdown", Qt::QueuedConnection, Q_ARG(bool, allow_confirm),
+		Q_ARG(bool, allow_save_state), Q_ARG(bool, default_save_state), Q_ARG(bool, false));
 }
 
 bool Host::IsFullscreen()
@@ -1254,7 +1244,8 @@ bool QtHost::InitializeConfig()
 		// If the config file doesn't exist, assume this is a new install and don't prompt to overwrite.
 		if (FileSystem::FileExists(s_base_settings_interface->GetFileName().c_str()) &&
 			QMessageBox::question(nullptr, QStringLiteral("PCSX2"),
-				QStringLiteral("Settings failed to load, or are the incorrect version. Clicking Yes will reset all settings to defaults. Do you want to continue?")) != QMessageBox::Yes)
+				QStringLiteral("Settings failed to load, or are the incorrect version. Clicking Yes will reset all settings to defaults. "
+							   "Do you want to continue?")) != QMessageBox::Yes)
 		{
 			return false;
 		}
@@ -1339,8 +1330,7 @@ bool QtHost::ShouldShowAdvancedSettings()
 void QtHost::RunOnUIThread(const std::function<void()>& func, bool block /*= false*/)
 {
 	// main window always exists, so it's fine to attach it to that.
-	QMetaObject::invokeMethod(g_main_window, "runOnUIThread",
-		block ? Qt::BlockingQueuedConnection : Qt::QueuedConnection,
+	QMetaObject::invokeMethod(g_main_window, "runOnUIThread", block ? Qt::BlockingQueuedConnection : Qt::QueuedConnection,
 		Q_ARG(const std::function<void()>&, func));
 }
 
@@ -1369,10 +1359,8 @@ QString QtHost::GetAppNameAndVersion()
 	else if constexpr (PCSX2_isReleaseVersion)
 	{
 #define APPNAME_STRINGIZE(x) #x
-		ret = QStringLiteral("PCSX2 "
-			APPNAME_STRINGIZE(PCSX2_VersionHi) "."
-			APPNAME_STRINGIZE(PCSX2_VersionMid) "."
-			APPNAME_STRINGIZE(PCSX2_VersionLo));
+		ret = QStringLiteral(
+			"PCSX2 " APPNAME_STRINGIZE(PCSX2_VersionHi) "." APPNAME_STRINGIZE(PCSX2_VersionMid) "." APPNAME_STRINGIZE(PCSX2_VersionLo));
 #undef APPNAME_STRINGIZE
 	}
 	else
@@ -1431,14 +1419,12 @@ void Host::ReportErrorAsync(const std::string_view& title, const std::string_vie
 {
 	if (!title.empty() && !message.empty())
 	{
-		Console.Error("ReportErrorAsync: %.*s: %.*s",
-			static_cast<int>(title.size()), title.data(),
-			static_cast<int>(message.size()), message.data());
+		Console.Error(
+			"ReportErrorAsync: %.*s: %.*s", static_cast<int>(title.size()), title.data(), static_cast<int>(message.size()), message.data());
 	}
 	else if (!message.empty())
 	{
-		Console.Error("ReportErrorAsync: %.*s",
-			static_cast<int>(message.size()), message.data());
+		Console.Error("ReportErrorAsync: %.*s", static_cast<int>(message.size()), message.data());
 	}
 
 	QMetaObject::invokeMethod(g_main_window, "reportError", Qt::QueuedConnection,
@@ -1455,9 +1441,7 @@ bool Host::ConfirmMessage(const std::string_view& title, const std::string_view&
 
 void Host::OpenURL(const std::string_view& url)
 {
-	QtHost::RunOnUIThread([url = QtUtils::StringViewToQString(url)]() {
-		QtUtils::OpenURL(g_main_window, QUrl(url));
-	});
+	QtHost::RunOnUIThread([url = QtUtils::StringViewToQString(url)]() { QtUtils::OpenURL(g_main_window, QUrl(url)); });
 }
 
 bool Host::CopyTextToClipboard(const std::string_view& text)
@@ -1493,15 +1477,13 @@ std::optional<WindowInfo> Host::GetTopLevelWindowInfo()
 
 void Host::OnInputDeviceConnected(const std::string_view& identifier, const std::string_view& device_name)
 {
-	emit g_emu_thread->onInputDeviceConnected(
-		identifier.empty() ? QString() : QString::fromUtf8(identifier.data(), identifier.size()),
+	emit g_emu_thread->onInputDeviceConnected(identifier.empty() ? QString() : QString::fromUtf8(identifier.data(), identifier.size()),
 		device_name.empty() ? QString() : QString::fromUtf8(device_name.data(), device_name.size()));
 }
 
 void Host::OnInputDeviceDisconnected(const std::string_view& identifier)
 {
-	emit g_emu_thread->onInputDeviceDisconnected(
-		identifier.empty() ? QString() : QString::fromUtf8(identifier.data(), identifier.size()));
+	emit g_emu_thread->onInputDeviceDisconnected(identifier.empty() ? QString() : QString::fromUtf8(identifier.data(), identifier.size()));
 }
 
 //////////////////////////////////////////////////////////////////////////
@@ -1746,9 +1728,9 @@ bool QtHost::ParseCommandLineOptions(const QStringList& args, std::shared_ptr<VM
 	// scanning the game list).
 	if (s_batch_mode && !s_start_fullscreen_ui && !autoboot)
 	{
-		QMessageBox::critical(nullptr, QStringLiteral("Error"), s_nogui_mode ?
-			QStringLiteral("Cannot use no-gui mode, because no boot filename was specified.") :
-			QStringLiteral("Cannot use batch mode, because no boot filename was specified."));
+		QMessageBox::critical(nullptr, QStringLiteral("Error"),
+			s_nogui_mode ? QStringLiteral("Cannot use no-gui mode, because no boot filename was specified.") :
+                           QStringLiteral("Cannot use batch mode, because no boot filename was specified."));
 		return false;
 	}
 

pcsx2-qt/QtHost.h
@@ -137,7 +137,7 @@ Q_SIGNALS:
 	void onVMStopped();
 
 	/// Provided by the host; called when the running executable changes.
-	void onGameChanged(const QString& path, const QString& serial, const QString& name, quint32 crc);
+	void onGameChanged(const QString& path, const QString& elf_override, const QString& serial, const QString& name, quint32 crc);
 
 	void onInputDevicesEnumerated(const QList<QPair<QString, QString>>& devices);
 	void onInputDeviceConnected(const QString& identifier, const QString& device_name);

pcsx2-qt/Settings/GameSummaryWidget.h
@@ -35,10 +35,15 @@ public:
 	~GameSummaryWidget();
 
 private:
-	void populateUi(const GameList::Entry* entry);
+	void populateInputProfiles();
+	void populateDetails(const GameList::Entry* entry);
+	void populateDiscPath(const GameList::Entry* entry);
 
 	void onInputProfileChanged(int index);
+	void onDiscPathChanged(const QString& value);
+	void onDiscPathBrowseClicked();
 
 	Ui::GameSummaryWidget m_ui;
 	SettingsDialog* m_dialog;
+	std::string m_entry_path;
 };

commit 1d5c11878a63bb04974367f38b1e4ca4d6948b1a

Qt: Fix crash opening ELF game properties
Introduced in 3197c95200551e880217f1e04ce694eddee5000e

pcsx2-qt/Settings/GameSummaryWidget.cpp
@@ -85,7 +85,7 @@ void GameSummaryWidget::populateDiscPath(const GameList::Entry* entry)
 	if (entry->type == GameList::EntryType::ELF)
 	{
 		std::optional<std::string> iso_path(m_dialog->getStringValue("EmuCore", "DiscPath", std::nullopt));
-		if (!iso_path->empty())
+		if (iso_path.has_value() && !iso_path->empty())
 			m_ui.discPath->setText(QString::fromStdString(iso_path.value()));
 
 		connect(m_ui.discPath, &QLineEdit::textChanged, this, &GameSummaryWidget::onDiscPathChanged);