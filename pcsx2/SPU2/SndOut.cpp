SgtBilko76/ARMSX2-Quest

commit fdf446923836912bca5e88094bbcf84e20ecfd3b

SPU2: Update WriteSamples function commented out code to work properly when enabled.

pcsx2/SPU2/SndOut.cpp
@@ -347,31 +347,30 @@ void SndBuffer::_WriteSamples(StereoOut32* bData, int nSamples)
 
 		s32 comp = 0;
 
-		if( SynchMode == 0 ) // TimeStrech on
+		if (EmuConfig.SPU2.SynchMode == Pcsx2Config::SPU2Options::SynchronizationMode::TimeStretch) // TimeStrech on
 		{
 			comp = timeStretchOverrun();
 		}
 		else
 		{
 			// Toss half the buffer plus whatever's being written anew:
-			comp = GetAlignedBufferSize( (m_size + nSamples ) / 16 );
-			if( comp > (m_size-SndOutPacketSize) ) comp = m_size-SndOutPacketSize;
+			comp = GetAlignedBufferSize((m_size + nSamples) / 16);
+			if (comp > (m_size - SndOutPacketSize))
+				comp = m_size - SndOutPacketSize;
 		}
 
 		_DropSamples_Internal(comp);
 
-		if( MsgOverruns() )
-			ConLog(" * SPU2 > Overrun Compensation (%d packets tossed)\n", comp / SndOutPacketSize );
-		lastPct = 0.0;		// normalize the timestretcher
+		if (SPU2::MsgOverruns())
+			SPU2::ConLog(" * SPU2 > Overrun Compensation (%d packets tossed)\n", comp / SndOutPacketSize);
+		lastPct = 0.0; // normalize the timestretcher
+		_WriteSamples_Safe(bData, nSamples);
 #else
 		if (SPU2::MsgOverruns())
 			SPU2::ConLog(" * SPU2 > Overrun! 1 packet tossed)\n");
 		lastPct = 0.0; // normalize the timestretcher
-		return;
 #endif
 	}
-
-	_WriteSamples_Safe(bData, nSamples);
 }
 
 bool SndBuffer::Init(const char* modname)

commit fa0d29d0607af63b36ba6a09a51d15409de40737

SPU2: Fix sound output
Regression from fdf446923836912bca5e88094bbcf84e20ecfd3b

pcsx2/SPU2/SndOut.cpp
@@ -364,13 +364,17 @@ void SndBuffer::_WriteSamples(StereoOut32* bData, int nSamples)
 		if (SPU2::MsgOverruns())
 			SPU2::ConLog(" * SPU2 > Overrun Compensation (%d packets tossed)\n", comp / SndOutPacketSize);
 		lastPct = 0.0; // normalize the timestretcher
-		_WriteSamples_Safe(bData, nSamples);
 #else
 		if (SPU2::MsgOverruns())
 			SPU2::ConLog(" * SPU2 > Overrun! 1 packet tossed)\n");
 		lastPct = 0.0; // normalize the timestretcher
+
+		// Toss the packet because we overran the buffer.
+		return;
 #endif
 	}
+
+	_WriteSamples_Safe(bData, nSamples);
 }
 
 bool SndBuffer::Init(const char* modname)