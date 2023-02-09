Ziemas/pcsx2

Implement thread listing for IOP
Abstracts away threads behind a common interface for both EE and IOP

pcsx2-qt/Debugger/Models/ThreadModel.cpp
@@ -27,10 +27,7 @@ ThreadModel::ThreadModel(DebugInterface& cpu, QObject* parent)
 
 int ThreadModel::rowCount(const QModelIndex&) const
 {
-	if (m_cpu.getCpuType() == BREAKPOINT_EE)
-		return getEEThreads().size();
-	else
-		return 0;
+	return m_cpu.GetThreadList().size();
 }
 
 int ThreadModel::columnCount(const QModelIndex&) const
@@ -40,66 +37,65 @@ int ThreadModel::columnCount(const QModelIndex&) const
 
 QVariant ThreadModel::data(const QModelIndex& index, int role) const
 {
+	const auto threads = m_cpu.GetThreadList();
+	auto* const thread = threads.at(index.row()).get();
+
 	if (role == Qt::DisplayRole)
 	{
-		const auto thread = getEEThreads().at(index.row());
-
 		switch (index.column())
 		{
 			case ThreadModel::ID:
-				return thread.tid;
+				return thread->TID();
 			case ThreadModel::PC:
 			{
-				if (thread.data.status == THS_RUN)
+				if (thread->Status() == ThreadStatus::THS_RUN)
 					return QtUtils::FilledQStringFromValue(m_cpu.getPC(), 16);
-				else
-					return QtUtils::FilledQStringFromValue(thread.data.entry, 16);
+
+				return QtUtils::FilledQStringFromValue(thread->PC(), 16);
 			}
 			case ThreadModel::ENTRY:
-				return QtUtils::FilledQStringFromValue(thread.data.entry_init, 16);
+				return QtUtils::FilledQStringFromValue(thread->EntryPoint(), 16);
 			case ThreadModel::PRIORITY:
-				return QString::number(thread.data.currentPriority);
+				return QString::number(thread->Priority());
 			case ThreadModel::STATE:
 			{
-				const auto& state = ThreadStateStrings.find(thread.data.status);
+				const auto& state = ThreadStateStrings.find(thread->Status());
 				if (state != ThreadStateStrings.end())
 					return state->second;
-				else
-					return tr("INVALID");
+
+				return tr("INVALID");
 			}
 			case ThreadModel::WAIT_TYPE:
 			{
-				const auto& waitType = ThreadWaitStrings.find(thread.data.waitType);
+				const auto& waitType = ThreadWaitStrings.find(thread->State());
 				if (waitType != ThreadWaitStrings.end())
 					return waitType->second;
-				else
-					return tr("INVALID");
+
+				return tr("INVALID");
 			}
 		}
 	}
 	else if (role == Qt::UserRole)
 	{
-		const auto thread = getEEThreads().at(index.row());
-
 		switch (index.column())
 		{
 			case ThreadModel::ID:
-				return thread.tid;
+				return thread->TID();
 			case ThreadModel::PC:
 			{
-				if (thread.data.status == THS_RUN)
+				if (thread->Status() == ThreadStatus::THS_RUN)
 					return m_cpu.getPC();
-				else
-					return thread.data.entry;
+
+				return thread->PC();
 			}
 			case ThreadModel::ENTRY:
-				return thread.data.entry_init;
+				return thread->EntryPoint();
 			case ThreadModel::PRIORITY:
-				return thread.data.currentPriority;
+				return thread->Priority();
 			case ThreadModel::STATE:
-				return thread.data.status;
+				return static_cast<u32>(thread->Status());
 			case ThreadModel::WAIT_TYPE:
-				return thread.data.waitType;
+				return static_cast<u32>(thread->State());
 			default:
 				return QVariant();
 		}

pcsx2/DebugTools/BiosDebugData.h
@@ -15,9 +15,24 @@
 
 #pragma once
 #include "common/Pcsx2Types.h"
+#include <memory>
+#include <vector>
 #include "ps2/BiosTools.h"
 
-struct EEInternalThread { // internal struct
+enum class ThreadStatus
+{
+	THS_BAD = 0x00,
+	THS_RUN = 0x01,
+	THS_READY = 0x02,
+	THS_WAIT = 0x04,
+	THS_SUSPEND = 0x08,
+	THS_WAIT_SUSPEND = 0x0C,
+	THS_DORMANT = 0x10,
+};
+
+
+struct EEInternalThread
+{ // internal struct
 	u32 prev;
 	u32 next;
 	int status;
@@ -40,26 +55,139 @@ struct EEInternalThread { // internal struct
 	u32 heap_base;
 };
 
-enum {
-	THS_BAD          = 0x00,
-	THS_RUN          = 0x01,
-	THS_READY        = 0x02,
-	THS_WAIT         = 0x04,
-	THS_SUSPEND      = 0x08,
-	THS_WAIT_SUSPEND = 0x0C,
-	THS_DORMANT      = 0x10,
-};
-
-enum {
-	WAIT_NONE       = 0,
-	WAIT_WAKEUP_REQ = 1,
-	WAIT_SEMA       = 2,
-};
-
-struct EEThread
+// Not the full struct, just what we care about
+struct IOPInternalThread
 {
-	int tid;
+	u32 tid;
+	u32 PC;
+	u32 stackTop;
+	u32 SavedSP;
+	u32 status;
+	u32 entrypoint;
+	u32 waitstate;
+	u32 initPriority;
+};
+
+enum class IOPWaitStatus
+{
+	TSW_SLEEP = 1,
+	TSW_DELAY = 2,
+	TSW_SEMA = 3,
+	TSW_EVENTFLAG = 4,
+	TSW_MBX = 5,
+	TSW_VPL = 6,
+	TSW_FPL = 7,
+};
+
+enum class EEWaitStatus
+{
+	WAIT_NONE = 0,
+	WAIT_WAKEUP_REQ = 1,
+	WAIT_SEMA = 2,
+};
+
+enum class WaitState
+{
+	NONE,
+	WAKEUP_REQ,
+	SEMA,
+	SLEEP,
+	DELAY,
+	EVENTFLAG,
+	MBOX,
+	VPOOL,
+	FIXPOOL,
+};
+
+class BiosThread
+{
+public:
+	virtual ~BiosThread() = default;
+	[[nodiscard]] virtual u32 TID() const = 0;
+	[[nodiscard]] virtual u32 PC() const = 0;
+	[[nodiscard]] virtual ThreadStatus Status() const = 0;
+	[[nodiscard]] virtual WaitState State() const = 0;
+	[[nodiscard]] virtual u32 EntryPoint() const = 0;
+	[[nodiscard]] virtual u32 StackTop() const = 0;
+	[[nodiscard]] virtual u32 Priority() const = 0;
+};
+
+class EEThread : public BiosThread
+{
+public:
+	EEThread(int tid, EEInternalThread th)
+		: tid(tid)
+		, data(th)
+	{
+	}
+	~EEThread() override = default;
+
+	[[nodiscard]] u32 TID() const override { return tid; };
+	[[nodiscard]] u32 PC() const override { return data.entry; };
+	[[nodiscard]] ThreadStatus Status() const override { return static_cast<ThreadStatus>(data.status); };
+	[[nodiscard]] WaitState State() const override
+	{
+		auto wait = static_cast<EEWaitStatus>(data.waitType);
+		switch (wait)
+		{
+			case EEWaitStatus::WAIT_NONE:
+				return WaitState::NONE;
+			case EEWaitStatus::WAIT_WAKEUP_REQ:
+				return WaitState::WAKEUP_REQ;
+			case EEWaitStatus::WAIT_SEMA:
+				return WaitState::SEMA;
+		}
+	};
+	[[nodiscard]] u32 EntryPoint() const override { return data.entry_init; };
+	[[nodiscard]] u32 StackTop() const override { return data.stack; };
+	[[nodiscard]] u32 Priority() const override { return data.currentPriority; };
+
+private:
+	u32 tid;
 	EEInternalThread data;
 };
 
-std::vector<EEThread> getEEThreads();
+class IOPThread : public BiosThread
+{
+public:
+	IOPThread(IOPInternalThread th)
+		: data(th)
+	{
+	}
+	~IOPThread() override = default;
+
+	[[nodiscard]] u32 TID() const override { return data.tid; };
+	[[nodiscard]] u32 PC() const override { return data.PC; };
+	[[nodiscard]] ThreadStatus Status() const override { return static_cast<ThreadStatus>(data.status); };
+	[[nodiscard]] WaitState State() const override
+	{
+		auto wait = static_cast<IOPWaitStatus>(data.waitstate);
+		switch (wait)
+		{
+			case IOPWaitStatus::TSW_DELAY:
+				return WaitState::DELAY;
+			case IOPWaitStatus::TSW_EVENTFLAG:
+				return WaitState::EVENTFLAG;
+			case IOPWaitStatus::TSW_SLEEP:
+				return WaitState::SLEEP;
+			case IOPWaitStatus::TSW_SEMA:
+				return WaitState::SEMA;
+			case IOPWaitStatus::TSW_MBX:
+				return WaitState::MBOX;
+			case IOPWaitStatus::TSW_VPL:
+				return WaitState::VPOOL;
+			case IOPWaitStatus::TSW_FPL:
+				return WaitState::FIXPOOL;
+		}
+		return WaitState::NONE;
+	};
+	[[nodiscard]] u32 EntryPoint() const override { return data.entrypoint; };
+	[[nodiscard]] u32 StackTop() const override { return data.stackTop; };
+	[[nodiscard]] u32 Priority() const override { return data.initPriority; };
+
+private:
+	IOPInternalThread data;
+};
+
+std::vector<std::unique_ptr<BiosThread>> getIOPThreads();
+std::vector<std::unique_ptr<BiosThread>> getEEThreads();

pcsx2/IopBios.cpp
@@ -20,8 +20,12 @@
 #include "R5900.h" // for g_GameStarted
 #include "IopBios.h"
 #include "IopMem.h"
+#include "iR3000A.h"
+#include "ps2/BiosTools.h"
+#include "DebugTools/SymbolMap.h"
 
 #include <ctype.h>
+#include <fmt/format.h>
 #include <string.h>
 #include <sys/stat.h>
 #include "common/FileSystem.h"
@@ -981,12 +985,84 @@ namespace R3000A
 		}
 	} // namespace sysmem
 
+	namespace excepman
+	{
+		void RegisterExceptionHandler_DEBUG()
+		{
+			DevCon.WriteLn(Color_Gray, "RegisterExceptionHandler: %d", a0);
+		}
+	}
+
 	namespace loadcore
 	{
+
+		// Get's the module list ptr when loading any module
+		u32 GetModList(u32 a0reg)
+		{
+			u32 lcptr = iopMemRead32(0x3f0);
+			u32 lcstring = irxFindLoadcore(lcptr);
+			u32 list = 0;
+
+			if (lcstring == 0)
+			{
+				list = lcptr - 0x20;
+			}
+			else
+			{
+				list = lcstring + 0x18;
+			}
+
+			return list;
+		}
+
+		// Gets the thread list ptr from thbase
+		u32 GetThreadList(u32 a0reg, u32 version)
+		{
+			// Function 3 returns the main thread manager struct
+			u32 function = iopMemRead32(a0reg + 0x20);
+
+			// read the lui
+			u32 thstruct = (iopMemRead32(function) & 0xFFFF) << 16;
+			thstruct |= iopMemRead32(function + 4) & 0xFFFF;
+
+			u32 list = thstruct + 0x42c;
+
+			if (version > 0x101)
+				list = thstruct + 0x430;
+
+			return list;
+		}
+
+		void LoadFuncs(u32 a0reg)
+		{
+
+			const std::string modname = iopMemReadString(a0reg + 12);
+			DevCon.WriteLn(Color_Gray, "RegisterLibraryEntries: %8.8s version %x.%02x", modname.data(), (unsigned)iopMemRead8(a0 + 9), (unsigned)iopMemRead8(a0 + 8));
+
+			u32 func = a0reg + 20;
+			u32 funcptr = iopMemRead32(func);
+			u32 index = 0;
+			while (funcptr != 0) {
+				const char* funcname = irxImportFuncname(modname, index);
+				R3000SymbolMap.AddFunction(fmt::format("{}::{}", modname, funcname).c_str(), funcptr, 0);
+				index++;
+				func += 4;
+				funcptr = iopMemRead32(func);
+			}
+		}
+
 		void RegisterLibraryEntries_DEBUG()
 		{
+			LoadFuncs(a0);
+
 			const std::string modname = iopMemReadString(a0 + 12);
-			DevCon.WriteLn(Color_Gray, "RegisterLibraryEntries: %8.8s version %x.%02x", modname.data(), (unsigned)iopMemRead8(a0 + 9), (unsigned)iopMemRead8(a0 + 8));
+			if (modname == "thbase")
+			{
+				const u32 version = iopMemRead32(a0 + 8);
+				CurrentBiosInformation.iopThreadListAddr = GetThreadList(a0, version);
+			}
+
+			CurrentBiosInformation.iopModListAddr = GetModList(a0);
 		}
 	} // namespace loadcore
 
@@ -1035,6 +1111,24 @@ namespace R3000A
 		}
 	} // namespace sifcmd
 
+	u32 irxFindLoadcore(u32 entrypc)
+	{
+		u32 i;
+
+		i = entrypc;
+		while (entrypc - i < 0x50)
+		{
+			// find loadcore string
+			if (iopMemRead32(i) == 0x49497350 && iopMemRead32(i+4) == 0x64616F6C)
+			{
+				return i;
+			}
+			i -= 4;
+		}
+
+		return 0;
+	}
+
 	u32 irxImportTableAddr(u32 entrypc)
 	{
 		u32 i;
@@ -1064,7 +1158,7 @@ namespace R3000A
 				// case 3: ???
 		}
 
-		return 0;
+		return "";
 	}
 
 // clang-format off
@@ -1130,6 +1224,9 @@ namespace R3000A
 	irxDEBUG irxImportDebug(const std::string& libname, u16 index)
 	{
 		// clang-format off
+		MODULE(excepman)
+			EXPORT_D(  4, RegisterExceptionHandler)
+		END_MODULE
 		MODULE(loadcore)
 			EXPORT_D(  6, RegisterLibraryEntries)
 		END_MODULE

SgtBilko76/ARMSX2-Quest

pcsx2-qt/Debugger/Models/ThreadModel.cpp
@@ -27,10 +27,7 @@ ThreadModel::ThreadModel(DebugInterface& cpu, QObject* parent)
 
 int ThreadModel::rowCount(const QModelIndex&) const
 {
-	if (m_cpu.getCpuType() == BREAKPOINT_EE)
-		return getEEThreads().size();
-	else
-		return 0;
+	return m_cpu.GetThreadList().size();
 }
 
 int ThreadModel::columnCount(const QModelIndex&) const
@@ -40,66 +37,65 @@ int ThreadModel::columnCount(const QModelIndex&) const
 
 QVariant ThreadModel::data(const QModelIndex& index, int role) const
 {
+	const auto threads = m_cpu.GetThreadList();
+	auto* const thread = threads.at(index.row()).get();
+
 	if (role == Qt::DisplayRole)
 	{
-		const auto thread = getEEThreads().at(index.row());
-
 		switch (index.column())
 		{
 			case ThreadModel::ID:
-				return thread.tid;
+				return thread->TID();
 			case ThreadModel::PC:
 			{
-				if (thread.data.status == THS_RUN)
+				if (thread->Status() == ThreadStatus::THS_RUN)
 					return QtUtils::FilledQStringFromValue(m_cpu.getPC(), 16);
-				else
-					return QtUtils::FilledQStringFromValue(thread.data.entry, 16);
+
+				return QtUtils::FilledQStringFromValue(thread->PC(), 16);
 			}
 			case ThreadModel::ENTRY:
-				return QtUtils::FilledQStringFromValue(thread.data.entry_init, 16);
+				return QtUtils::FilledQStringFromValue(thread->EntryPoint(), 16);
 			case ThreadModel::PRIORITY:
-				return QString::number(thread.data.currentPriority);
+				return QString::number(thread->Priority());
 			case ThreadModel::STATE:
 			{
-				const auto& state = ThreadStateStrings.find(thread.data.status);
+				const auto& state = ThreadStateStrings.find(thread->Status());
 				if (state != ThreadStateStrings.end())
 					return state->second;
-				else
-					return tr("INVALID");
+
+				return tr("INVALID");
 			}
 			case ThreadModel::WAIT_TYPE:
 			{
-				const auto& waitType = ThreadWaitStrings.find(thread.data.waitType);
+				const auto& waitType = ThreadWaitStrings.find(thread->Wait());
 				if (waitType != ThreadWaitStrings.end())
 					return waitType->second;
-				else
-					return tr("INVALID");
+
+				return tr("INVALID");
 			}
 		}
 	}
 	else if (role == Qt::UserRole)
 	{
-		const auto thread = getEEThreads().at(index.row());
-
 		switch (index.column())
 		{
 			case ThreadModel::ID:
-				return thread.tid;
+				return thread->TID();
 			case ThreadModel::PC:
 			{
-				if (thread.data.status == THS_RUN)
+				if (thread->Status() == ThreadStatus::THS_RUN)
 					return m_cpu.getPC();
-				else
-					return thread.data.entry;
+
+				return thread->PC();
 			}
 			case ThreadModel::ENTRY:
-				return thread.data.entry_init;
+				return thread->EntryPoint();
 			case ThreadModel::PRIORITY:
-				return thread.data.currentPriority;
+				return thread->Priority();
 			case ThreadModel::STATE:
-				return thread.data.status;
+				return static_cast<u32>(thread->Status());
 			case ThreadModel::WAIT_TYPE:
-				return thread.data.waitType;
+				return static_cast<u32>(thread->Wait());
 			default:
 				return QVariant();
 		}

pcsx2/DebugTools/BiosDebugData.h
@@ -15,9 +15,24 @@
 
 #pragma once
 #include "common/Pcsx2Types.h"
+#include <memory>
+#include <vector>
 #include "ps2/BiosTools.h"
 
-struct EEInternalThread { // internal struct
+enum class ThreadStatus
+{
+	THS_BAD = 0x00,
+	THS_RUN = 0x01,
+	THS_READY = 0x02,
+	THS_WAIT = 0x04,
+	THS_SUSPEND = 0x08,
+	THS_WAIT_SUSPEND = 0x0C,
+	THS_DORMANT = 0x10,
+};
+
+
+struct EEInternalThread
+{ // internal struct
 	u32 prev;
 	u32 next;
 	int status;
@@ -40,26 +55,139 @@ struct EEInternalThread { // internal struct
 	u32 heap_base;
 };
 
-enum {
-	THS_BAD          = 0x00,
-	THS_RUN          = 0x01,
-	THS_READY        = 0x02,
-	THS_WAIT         = 0x04,
-	THS_SUSPEND      = 0x08,
-	THS_WAIT_SUSPEND = 0x0C,
-	THS_DORMANT      = 0x10,
-};
-
-enum {
-	WAIT_NONE       = 0,
-	WAIT_WAKEUP_REQ = 1,
-	WAIT_SEMA       = 2,
-};
-
-struct EEThread
+// Not the full struct, just what we care about
+struct IOPInternalThread
 {
-	int tid;
+	u32 tid;
+	u32 PC;
+	u32 stackTop;
+	u32 SavedSP;
+	u32 status;
+	u32 entrypoint;
+	u32 waitstate;
+	u32 initPriority;
+};
+
+enum class IOPWaitStatus
+{
+	TSW_SLEEP = 1,
+	TSW_DELAY = 2,
+	TSW_SEMA = 3,
+	TSW_EVENTFLAG = 4,
+	TSW_MBX = 5,
+	TSW_VPL = 6,
+	TSW_FPL = 7,
+};
+
+enum class EEWaitStatus
+{
+	WAIT_NONE = 0,
+	WAIT_WAKEUP_REQ = 1,
+	WAIT_SEMA = 2,
+};
+
+enum class WaitState
+{
+	NONE,
+	WAKEUP_REQ,
+	SEMA,
+	SLEEP,
+	DELAY,
+	EVENTFLAG,
+	MBOX,
+	VPOOL,
+	FIXPOOL,
+};
+
+class BiosThread
+{
+public:
+	virtual ~BiosThread() = default;
+	[[nodiscard]] virtual u32 TID() const = 0;
+	[[nodiscard]] virtual u32 PC() const = 0;
+	[[nodiscard]] virtual ThreadStatus Status() const = 0;
+	[[nodiscard]] virtual WaitState Wait() const = 0;
+	[[nodiscard]] virtual u32 EntryPoint() const = 0;
+	[[nodiscard]] virtual u32 StackTop() const = 0;
+	[[nodiscard]] virtual u32 Priority() const = 0;
+};
+
+class EEThread : public BiosThread
+{
+public:
+	EEThread(int tid, EEInternalThread th)
+		: tid(tid)
+		, data(th)
+	{
+	}
+	~EEThread() override = default;
+
+	[[nodiscard]] u32 TID() const override { return tid; };
+	[[nodiscard]] u32 PC() const override { return data.entry; };
+	[[nodiscard]] ThreadStatus Status() const override { return static_cast<ThreadStatus>(data.status); };
+	[[nodiscard]] WaitState Wait() const override
+	{
+		auto wait = static_cast<EEWaitStatus>(data.waitType);
+		switch (wait)
+		{
+			case EEWaitStatus::WAIT_NONE:
+				return WaitState::NONE;
+			case EEWaitStatus::WAIT_WAKEUP_REQ:
+				return WaitState::WAKEUP_REQ;
+			case EEWaitStatus::WAIT_SEMA:
+				return WaitState::SEMA;
+		}
+	};
+	[[nodiscard]] u32 EntryPoint() const override { return data.entry_init; };
+	[[nodiscard]] u32 StackTop() const override { return data.stack; };
+	[[nodiscard]] u32 Priority() const override { return data.currentPriority; };
+
+private:
+	u32 tid;
 	EEInternalThread data;
 };
 
-std::vector<EEThread> getEEThreads();
+class IOPThread : public BiosThread
+{
+public:
+	IOPThread(IOPInternalThread th)
+		: data(th)
+	{
+	}
+	~IOPThread() override = default;
+
+	[[nodiscard]] u32 TID() const override { return data.tid; };
+	[[nodiscard]] u32 PC() const override { return data.PC; };
+	[[nodiscard]] ThreadStatus Status() const override { return static_cast<ThreadStatus>(data.status); };
+	[[nodiscard]] WaitState Wait() const override
+	{
+		auto wait = static_cast<IOPWaitStatus>(data.waitstate);
+		switch (wait)
+		{
+			case IOPWaitStatus::TSW_DELAY:
+				return WaitState::DELAY;
+			case IOPWaitStatus::TSW_EVENTFLAG:
+				return WaitState::EVENTFLAG;
+			case IOPWaitStatus::TSW_SLEEP:
+				return WaitState::SLEEP;
+			case IOPWaitStatus::TSW_SEMA:
+				return WaitState::SEMA;
+			case IOPWaitStatus::TSW_MBX:
+				return WaitState::MBOX;
+			case IOPWaitStatus::TSW_VPL:
+				return WaitState::VPOOL;
+			case IOPWaitStatus::TSW_FPL:
+				return WaitState::FIXPOOL;
+		}
+		return WaitState::NONE;
+	};
+	[[nodiscard]] u32 EntryPoint() const override { return data.entrypoint; };
+	[[nodiscard]] u32 StackTop() const override { return data.stackTop; };
+	[[nodiscard]] u32 Priority() const override { return data.initPriority; };
+
+private:
+	IOPInternalThread data;
+};
+
+std::vector<std::unique_ptr<BiosThread>> getIOPThreads();
+std::vector<std::unique_ptr<BiosThread>> getEEThreads();

pcsx2/IopBios.cpp
@@ -20,8 +20,12 @@
 #include "R5900.h" // for g_GameStarted
 #include "IopBios.h"
 #include "IopMem.h"
+#include "iR3000A.h"
+#include "ps2/BiosTools.h"
+#include "DebugTools/SymbolMap.h"
 
 #include <ctype.h>
+#include <fmt/format.h>
 #include <string.h>
 #include <sys/stat.h>
 #include "common/FileSystem.h"
@@ -983,6 +987,37 @@ namespace R3000A
 
 	namespace loadcore
 	{
+
+		// Gets the thread list ptr from thbase
+		u32 GetThreadList(u32 a0reg, u32 version)
+		{
+			// Function 3 returns the main thread manager struct
+			u32 function = iopMemRead32(a0reg + 0x20);
+
+			// read the lui
+			u32 thstruct = (iopMemRead32(function) & 0xFFFF) << 16;
+			thstruct |= iopMemRead32(function + 4) & 0xFFFF;
+
+			u32 list = thstruct + 0x42c;
+
+			if (version > 0x101)
+				list = thstruct + 0x430;
+
+			return list;
+		}
+
+		int RegisterLibraryEntries_HLE()
+		{
+			const std::string modname = iopMemReadString(a0 + 12);
+			if (modname == "thbase")
+			{
+				const u32 version = iopMemRead32(a0 + 8);
+				CurrentBiosInformation.iopThreadListAddr = GetThreadList(a0, version);
+			}
+
+			return 0;
+		}
+
 		void RegisterLibraryEntries_DEBUG()
 		{
 			const std::string modname = iopMemReadString(a0 + 12);
@@ -1091,6 +1126,11 @@ namespace R3000A
 		// clang-format off
 		MODULE(sysmem)
 			EXPORT_H( 14, Kprintf)
+		END_MODULE
+
+		// For grabbing the thread list from thbase
+		MODULE(loadcore)
+			EXPORT_H( 6, RegisterLibraryEntries)
 		END_MODULE
 
 		// Special case with ioman and iomanX