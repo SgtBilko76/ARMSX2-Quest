TellowKrinkle/pcsx2

Qt: Add option to prefer English titles in Game List

pcsx2-qt/MainWindow.cpp
@@ -2272,6 +2272,9 @@ SettingsDialog* MainWindow::getSettingsDialog()
 				g_main_window->doSettings("Interface");
 			});
 		});
+		connect(m_settings_dialog->getGameListSettingsWidget(), &GameListSettingsWidget::preferEnglishGameListChanged, this, []{
+			g_main_window->m_game_list_widget->refreshGridCovers();
+		});
 	}
 
 	return m_settings_dialog;

SgtBilko76/ARMSX2-Quest

pcsx2-qt/MainWindow.cpp
@@ -2299,6 +2299,9 @@ SettingsWindow* MainWindow::getSettingsWindow()
 				g_main_window->doSettings("Interface");
 			});
 		});
+		connect(m_setings_window->getGameListSettingsWidget(), &GameListSettingsWidget::preferEnglishGameListChanged, this, []{
+			g_main_window->m_game_list_widget->refreshGridCovers();
+		});
 	}
 
 	return m_setings_window;