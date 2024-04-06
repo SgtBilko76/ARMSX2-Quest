PCSX2/pcsx2

Build: Unify shared libraries across platforms
Use DLLs/shared libraries for:
 - libjpeg
 - liblzma
 - libpng
 - libwebp
 - lz4
 - SDL
 - zlib
 - zstd

cmake/SearchForStuff.cmake
@@ -1,17 +1,26 @@
 #-------------------------------------------------------------------------------
 #                       Search all libraries on the system
 #-------------------------------------------------------------------------------
-if(EXISTS ${PROJECT_SOURCE_DIR}/.git)
-	find_package(Git)
-endif()
+find_package(Git)
+
+# Require threads on all OSes.
+find_package(Threads REQUIRED)
+
+# Dependency libraries.
+# On macOS, Mono.framework contains an ancient version of libpng.  We don't want that.
+# Avoid it by telling cmake to avoid finding frameworks while we search for libpng.
+set(FIND_FRAMEWORK_BACKUP ${CMAKE_FIND_FRAMEWORK})
+set(CMAKE_FIND_FRAMEWORK NEVER)
+find_package(PNG 1.6.40 REQUIRED)
+find_package(ZLIB REQUIRED) # v1.3, but Mac uses the SDK version.
+find_package(Zstd 1.5.5 REQUIRED)
+find_package(LibLZMA REQUIRED)
+find_package(LZ4 REQUIRED)
+find_package(WebP REQUIRED) # v1.3.2, spews an error on Linux because no pkg-config.
+find_package(SDL2 2.30.1 REQUIRED)
+
+# Platform-specific dependencies.
 if (WIN32)
-	# We bundle everything on Windows
-	add_subdirectory(3rdparty/zlib EXCLUDE_FROM_ALL)
-	add_subdirectory(3rdparty/libpng EXCLUDE_FROM_ALL)
-	add_subdirectory(3rdparty/libwebp EXCLUDE_FROM_ALL)
-	add_subdirectory(3rdparty/xz EXCLUDE_FROM_ALL)
-	add_subdirectory(3rdparty/zstd EXCLUDE_FROM_ALL)
-	add_subdirectory(3rdparty/lz4 EXCLUDE_FROM_ALL)
 	add_subdirectory(3rdparty/D3D12MemAlloc EXCLUDE_FROM_ALL)
 	add_subdirectory(3rdparty/winpixeventruntime EXCLUDE_FROM_ALL)
 	set(FFMPEG_INCLUDE_DIRS "${CMAKE_SOURCE_DIR}/3rdparty/ffmpeg/include")
@@ -22,9 +31,8 @@ if (WIN32)
 	set(WIL_BUILD_PACKAGING OFF CACHE BOOL "")
 	add_subdirectory(3rdparty/wil EXCLUDE_FROM_ALL)
 else()
+	find_package(CURL REQUIRED)
 	find_package(PCAP REQUIRED)
-	find_package(LibLZMA REQUIRED)
-	make_imported_target_if_missing(LibLZMA::LibLZMA LIBLZMA)
 
 	# Using find_package OpenGL without either setting your opengl preference to GLVND or LEGACY
 	# is deprecated as of cmake 3.11.
@@ -32,13 +40,6 @@ else()
 		set(OpenGL_GL_PREFERENCE GLVND)
 		find_package(OpenGL REQUIRED)
 	endif()
-	# On macOS, Mono.framework contains an ancient version of libpng.  We don't want that.
-	# Avoid it by telling cmake to avoid finding frameworks while we search for libpng.
-	set(FIND_FRAMEWORK_BACKUP ${CMAKE_FIND_FRAMEWORK})
-	set(CMAKE_FIND_FRAMEWORK NEVER)
-	find_package(PNG REQUIRED)
-	find_package(CURL REQUIRED)
-	set(CMAKE_FIND_FRAMEWORK ${FIND_FRAMEWORK_BACKUP})
 	find_package(Vtune)
 
 	# Use bundled ffmpeg v4.x.x headers if we can't locate it in the system.
@@ -48,11 +49,6 @@ else()
 		message(WARNING "FFmpeg not found, using bundled headers.")
 		set(FFMPEG_INCLUDE_DIRS "${CMAKE_SOURCE_DIR}/3rdparty/ffmpeg/include")
 	endif()
-
-	find_package(ZLIB REQUIRED)
-	find_package(Zstd REQUIRED)
-	find_package(LZ4 REQUIRED)
-	find_package(WebP REQUIRED)
 
 	## Use CheckLib package to find module
 	include(CheckLib)
@@ -84,13 +80,9 @@ else()
 		find_package(PkgConfig REQUIRED)
 		pkg_check_modules(DBUS REQUIRED dbus-1)
 	endif()
-endif(WIN32)
+endif()
 
-# Require threads on all OSes.
-find_package(Threads REQUIRED)
-
-# Also need SDL2.
-find_package(SDL2 2.30.1 REQUIRED)
+set(CMAKE_FIND_FRAMEWORK ${FIND_FRAMEWORK_BACKUP})
 
 set(ACTUALLY_ENABLE_TESTS ${ENABLE_TESTS})
 if(ENABLE_TESTS)

SgtBilko76/ARMSX2-Quest

cmake/SearchForStuff.cmake
@@ -1,16 +1,25 @@
 #-------------------------------------------------------------------------------
 #                       Search all libraries on the system
 #-------------------------------------------------------------------------------
-if(EXISTS ${PROJECT_SOURCE_DIR}/.git)
-	find_package(Git)
-endif()
+find_package(Git)
+
+# Require threads on all OSes.
+find_package(Threads REQUIRED)
+
+# Dependency libraries.
+# On macOS, Mono.framework contains an ancient version of libpng.  We don't want that.
+# Avoid it by telling cmake to avoid finding frameworks while we search for libpng.
+set(FIND_FRAMEWORK_BACKUP ${CMAKE_FIND_FRAMEWORK})
+set(CMAKE_FIND_FRAMEWORK NEVER)
+find_package(PNG 1.6.40 REQUIRED)
+find_package(ZLIB REQUIRED) # v1.3, but Mac uses the SDK version.
+find_package(Zstd 1.5.5 REQUIRED)
+find_package(LZ4 REQUIRED)
+find_package(WebP REQUIRED) # v1.3.2, spews an error on Linux because no pkg-config.
+find_package(SDL2 2.30.2 REQUIRED)
+
+# Platform-specific dependencies.
 if (WIN32)
-	# We bundle everything on Windows
-	add_subdirectory(3rdparty/zlib EXCLUDE_FROM_ALL)
-	add_subdirectory(3rdparty/libpng EXCLUDE_FROM_ALL)
-	add_subdirectory(3rdparty/libwebp EXCLUDE_FROM_ALL)
-	add_subdirectory(3rdparty/zstd EXCLUDE_FROM_ALL)
-	add_subdirectory(3rdparty/lz4 EXCLUDE_FROM_ALL)
 	add_subdirectory(3rdparty/D3D12MemAlloc EXCLUDE_FROM_ALL)
 	add_subdirectory(3rdparty/winpixeventruntime EXCLUDE_FROM_ALL)
 	set(FFMPEG_INCLUDE_DIRS "${CMAKE_SOURCE_DIR}/3rdparty/ffmpeg/include")
@@ -21,21 +30,8 @@ if (WIN32)
 	set(WIL_BUILD_PACKAGING OFF CACHE BOOL "")
 	add_subdirectory(3rdparty/wil EXCLUDE_FROM_ALL)
 else()
-	find_package(PCAP REQUIRED)
-
-	# Using find_package OpenGL without either setting your opengl preference to GLVND or LEGACY
-	# is deprecated as of cmake 3.11.
-	if(USE_OPENGL)
-		set(OpenGL_GL_PREFERENCE GLVND)
-		find_package(OpenGL REQUIRED)
-	endif()
-	# On macOS, Mono.framework contains an ancient version of libpng.  We don't want that.
-	# Avoid it by telling cmake to avoid finding frameworks while we search for libpng.
-	set(FIND_FRAMEWORK_BACKUP ${CMAKE_FIND_FRAMEWORK})
-	set(CMAKE_FIND_FRAMEWORK NEVER)
-	find_package(PNG REQUIRED)
 	find_package(CURL REQUIRED)
-	set(CMAKE_FIND_FRAMEWORK ${FIND_FRAMEWORK_BACKUP})
+	find_package(PCAP REQUIRED)
 	find_package(Vtune)
 
 	# Use bundled ffmpeg v4.x.x headers if we can't locate it in the system.
@@ -45,11 +41,6 @@ else()
 		message(WARNING "FFmpeg not found, using bundled headers.")
 		set(FFMPEG_INCLUDE_DIRS "${CMAKE_SOURCE_DIR}/3rdparty/ffmpeg/include")
 	endif()
-
-	find_package(ZLIB REQUIRED)
-	find_package(Zstd REQUIRED)
-	find_package(LZ4 REQUIRED)
-	find_package(WebP REQUIRED)
 
 	## Use CheckLib package to find module
 	include(CheckLib)
@@ -81,13 +72,9 @@ else()
 		find_package(PkgConfig REQUIRED)
 		pkg_check_modules(DBUS REQUIRED dbus-1)
 	endif()
-endif(WIN32)
+endif()
 
-# Require threads on all OSes.
-find_package(Threads REQUIRED)
-
-# Also need SDL2.
-find_package(SDL2 2.30.2 REQUIRED)
+set(CMAKE_FIND_FRAMEWORK ${FIND_FRAMEWORK_BACKUP})
 
 set(ACTUALLY_ENABLE_TESTS ${ENABLE_TESTS})
 if(ENABLE_TESTS)