TJnotJT/pcsx2

GS: Add dumping of transfer bitmaps.

pcsx2-gsrunner/Main.cpp
@@ -473,8 +473,8 @@ static void PrintCommandLineHelp(const char* progname)
 	std::fprintf(stderr, "  -help: Displays this information and exits.\n");
 	std::fprintf(stderr, "  -version: Displays version information and exits.\n");
 	std::fprintf(stderr, "  -dumpdir <dir>: Frame dump directory (will be dumped as filename_frameN.png).\n");
-	std::fprintf(stderr, "  -dump [rt|tex|z|f|a|i]: Enabling dumping of render target, texture, z buffer, frame, "
-		"alphas, and info (context, vertices), respectively, per draw. Generates lots of data.\n");
+	std::fprintf(stderr, "  -dump [rt|tex|z|f|a|i|tr]: Enabling dumping of render target, texture, z buffer, frame, "
+		"alphas, and info (context, vertices, transfers (list)), transfers (images), respectively, per draw. Generates lots of data.\n");
 	std::fprintf(stderr, "  -dumprange N[,L,B]: Start dumping from draw N (base 0), stops after L draws, and only "
 		"those draws that are multiples of B (intersection of -dumprange and -dumprangef used)."
 		"Defaults to 0,-1,1 (all draws). Only used if -dump used.\n");
@@ -558,6 +558,8 @@ bool GSRunner::ParseCommandLineArgs(int argc, char* argv[], VMBootParameters& pa
 					s_settings_interface.SetBoolValue("EmuCore/GS", "SaveAlpha", true);
 				if (str.find("i") != std::string::npos)
 					s_settings_interface.SetBoolValue("EmuCore/GS", "SaveInfo", true);
+				if (str.find("tr") != std::string::npos)
+					s_settings_interface.SetBoolValue("EmuCore/GS", "SaveTransfers", true);
 				continue;
 			}
 			else if (CHECK_ARG_PARAM("-dumprange"))

pcsx2-qt/Settings/DebugSettingsWidget.cpp
@@ -66,6 +66,7 @@ DebugSettingsWidget::DebugSettingsWidget(SettingsWindow* dialog, QWidget* parent
 	SettingWidgetBinder::BindWidgetToBoolSetting(sif, m_ui.saveDepth, "EmuCore/GS", "SaveDepth", false);
 	SettingWidgetBinder::BindWidgetToBoolSetting(sif, m_ui.saveAlpha, "EmuCore/GS", "SaveAlpha", false);
 	SettingWidgetBinder::BindWidgetToBoolSetting(sif, m_ui.saveInfo, "EmuCore/GS", "SaveInfo", false);
+	SettingWidgetBinder::BindWidgetToBoolSetting(sif, m_ui.saveTransfers, "EmuCore/GS", "SaveTransfers", false);
 	SettingWidgetBinder::BindWidgetToIntSetting(sif, m_ui.saveDrawStart, "EmuCore/GS", "SaveDrawStart", 0);
 	SettingWidgetBinder::BindWidgetToIntSetting(sif, m_ui.saveDrawCount, "EmuCore/GS", "SaveDrawCount", 5000);
 	SettingWidgetBinder::BindWidgetToIntSetting(sif, m_ui.saveFrameStart, "EmuCore/GS", "SaveFrameStart", 0);
@@ -172,6 +173,7 @@ void DebugSettingsWidget::onDrawDumpingChanged()
 	m_ui.saveDepth->setEnabled(enabled);
 	m_ui.saveAlpha->setEnabled(enabled);
 	m_ui.saveInfo->setEnabled(enabled);
+	m_ui.saveTransfers->setEnabled(enabled);
 	m_ui.saveDrawStart->setEnabled(enabled);
 	m_ui.saveDrawCount->setEnabled(enabled);
 	m_ui.saveFrameStart->setEnabled(enabled);

pcsx2/GS/GSState.cpp
@@ -15,6 +15,7 @@
 #include <algorithm>
 #include <cfloat>
 #include <fstream>
+#include <sstream>
 #include <iomanip>
 #include <bit>
 
@@ -457,7 +458,7 @@ void GSState::DumpDrawInfo(bool dump_regs, bool dump_verts, bool dump_transfers)
 	if (dump_transfers)
 	{
 		s = GetDrawDumpPath("%05d_transfers.txt", s_n);
-		DumpTransfers(s);
+		DumpTransferList(s);
 	}
 }
 
@@ -719,7 +720,7 @@ void GSState::DumpVertices(const std::string& filename)
 	}
 }
 
-void GSState::DumpTransfers(const std::string& filename)
+void GSState::DumpTransferList(const std::string& filename)
 {
 	// Only create the file if there are transfers to dump
 	std::optional<std::ofstream> file;
@@ -767,11 +768,46 @@ void GSState::DumpTransfers(const std::string& filename)
 		(*file) << INDENT << "rect: [" << std::dec << transfer.rect.x << DEL << transfer.rect.y << DEL <<
 			transfer.rect.z << DEL << transfer.rect.w << "]" << std::endl;
 
-		// Dump draw number
-		(*file) << INDENT << "draw: " << std::dec << transfer.draw << std::endl;
-
 		// Dump zero_clear
 		(*file) << INDENT << "zero_clear: " << (transfer.zero_clear ? "true" : "false") << std::endl;
+	}
+}
+
+void GSState::DumpTransferImages()
+{
+	// Only create the file if there are transfers to dump
+	std::optional<std::ofstream> file;
+
+	constexpr const char* DEL = "_";
+	constexpr const char* INDENT = "  ";
+
+	int transfer_n = 0;
+	for (int i = 0; i < static_cast<int>(m_draw_transfers.size()); ++i)
+	{
+		if (m_draw_transfers[i].draw != s_n - 1)
+			continue; // skip transfers that did not start in the previous draw
+
+		const GSUploadQueue& transfer = m_draw_transfers[i];
+
+		std::string filename;
+		if (transfer.ee_to_gs)
+		{
+			// Transferring GS->GS then only the destination info is relevant.
+			filename = GetDrawDumpPath("%05d_xfer%02d_EE_to_GS_%03x_%d_%s_%d_%d_%d_%d.png",
+				s_n, transfer_n++, transfer.blit.DBP, transfer.blit.DBW, GSUtil::GetPSMName(transfer.blit.DPSM),
+				transfer.rect.x, transfer.rect.y, transfer.rect.z, transfer.rect.w);
+		}
+		else
+		{
+			// Transferring GS->GS then the source info is relevant.
+			filename = GetDrawDumpPath("%05d_xfer%02d_EE_to_GS_%03x_%d_%s_%03x_%d_%s_%d_%d_%d_%d.bmp",
+				s_n, transfer_n++, transfer.blit.SBP, transfer.blit.SBW, GSUtil::GetPSMName(transfer.blit.SPSM),
+				transfer.blit.DBP, transfer.blit.DBW, GSUtil::GetPSMName(transfer.blit.DPSM),
+				transfer.rect.x, transfer.rect.y, transfer.rect.z, transfer.rect.w);
+		}
+
+		m_mem.SaveBMP(filename, transfer.blit.DBP, transfer.blit.DBW, transfer.blit.DPSM,
+			transfer.rect.width(), transfer.rect.height(), transfer.rect.x, transfer.rect.y);
 	}
 }
 
@@ -2002,12 +2038,18 @@ void GSState::FlushPrim()
 		const bool skip_draw = (m_context->TEST.ZTE && m_context->TEST.ZTST == ZTST_NEVER);
 		m_quad_check_valid = false;
 
-		if (GSConfig.SaveInfo && GSConfig.ShouldDump(s_n, g_perfmon.GetFrame()))
+		if (GSConfig.ShouldDump(s_n, g_perfmon.GetFrame()))
 		{
-			// Only dump registers/vertices if we are drawing.
-			// Always dump the transfers since these are relevant for debugging regardless of
-			// whether the draw is skipped or not.
-			DumpDrawInfo(!skip_draw, !skip_draw, true);
+			if (GSConfig.SaveInfo)
+			{
+				// Only dump registers/vertices if we are drawing.
+				// Always dump the transfers since these are relevant for debugging regardless of
+				// whether the draw is skipped or not.
+				DumpDrawInfo(!skip_draw, !skip_draw, true);
+			}
+
+			if (GSConfig.SaveTransfers)
+				DumpTransferImages();
 		}
 
 		if (!skip_draw)

SgtBilko76/ARMSX2-Quest

pcsx2-gsrunner/Main.cpp
@@ -473,8 +473,8 @@ static void PrintCommandLineHelp(const char* progname)
 	std::fprintf(stderr, "  -help: Displays this information and exits.\n");
 	std::fprintf(stderr, "  -version: Displays version information and exits.\n");
 	std::fprintf(stderr, "  -dumpdir <dir>: Frame dump directory (will be dumped as filename_frameN.png).\n");
-	std::fprintf(stderr, "  -dump [rt|tex|z|f|a|i]: Enabling dumping of render target, texture, z buffer, frame, "
-		"alphas, and info (context, vertices), respectively, per draw. Generates lots of data.\n");
+	std::fprintf(stderr, "  -dump [rt|tex|z|f|a|i|tr]: Enabling dumping of render target, texture, z buffer, frame, "
+		"alphas, and info (context, vertices, transfers (list)), transfers (images), respectively, per draw. Generates lots of data.\n");
 	std::fprintf(stderr, "  -dumprange N[,L,B]: Start dumping from draw N (base 0), stops after L draws, and only "
 		"those draws that are multiples of B (intersection of -dumprange and -dumprangef used)."
 		"Defaults to 0,-1,1 (all draws). Only used if -dump used.\n");
@@ -558,6 +558,8 @@ bool GSRunner::ParseCommandLineArgs(int argc, char* argv[], VMBootParameters& pa
 					s_settings_interface.SetBoolValue("EmuCore/GS", "SaveAlpha", true);
 				if (str.find("i") != std::string::npos)
 					s_settings_interface.SetBoolValue("EmuCore/GS", "SaveInfo", true);
+				if (str.find("tr") != std::string::npos)
+					s_settings_interface.SetBoolValue("EmuCore/GS", "SaveTransferImages", true);
 				continue;
 			}
 			else if (CHECK_ARG_PARAM("-dumprange"))

pcsx2-qt/Settings/DebugSettingsWidget.cpp
@@ -109,6 +109,7 @@ DebugSettingsWidget::DebugSettingsWidget(SettingsWindow* settings_dialog, QWidge
 	SettingWidgetBinder::BindWidgetToBoolSetting(sif, m_gs.saveDepth, "EmuCore/GS", "SaveDepth", false);
 	SettingWidgetBinder::BindWidgetToBoolSetting(sif, m_gs.saveAlpha, "EmuCore/GS", "SaveAlpha", false);
 	SettingWidgetBinder::BindWidgetToBoolSetting(sif, m_gs.saveInfo, "EmuCore/GS", "SaveInfo", false);
+	SettingWidgetBinder::BindWidgetToBoolSetting(sif, m_gs.saveTransferImages, "EmuCore/GS", "SaveTransferImages", false);
 	SettingWidgetBinder::BindWidgetToIntSetting(sif, m_gs.saveDrawStart, "EmuCore/GS", "SaveDrawStart", 0);
 	SettingWidgetBinder::BindWidgetToIntSetting(sif, m_gs.saveDrawCount, "EmuCore/GS", "SaveDrawCount", 5000);
 	SettingWidgetBinder::BindWidgetToIntSetting(sif, m_gs.saveFrameStart, "EmuCore/GS", "SaveFrameStart", 0);
@@ -213,6 +214,7 @@ void DebugSettingsWidget::onDrawDumpingChanged()
 	m_gs.saveDepth->setEnabled(enabled);
 	m_gs.saveAlpha->setEnabled(enabled);
 	m_gs.saveInfo->setEnabled(enabled);
+	m_gs.saveTransferImages->setEnabled(enabled);
 	m_gs.saveDrawStart->setEnabled(enabled);
 	m_gs.saveDrawCount->setEnabled(enabled);
 	m_gs.saveFrameStart->setEnabled(enabled);

pcsx2/GS/GSState.cpp
@@ -15,6 +15,7 @@
 #include <algorithm>
 #include <cfloat>
 #include <fstream>
+#include <sstream>
 #include <iomanip>
 #include <bit>
 
@@ -457,7 +458,7 @@ void GSState::DumpDrawInfo(bool dump_regs, bool dump_verts, bool dump_transfers)
 	if (dump_transfers)
 	{
 		s = GetDrawDumpPath("%05d_transfers.txt", s_n);
-		DumpTransfers(s);
+		DumpTransferList(s);
 	}
 }
 
@@ -729,7 +730,7 @@ void GSState::DumpVertices(const std::string& filename)
 	file << CLOSE_MAP << std::endl;
 }
 
-void GSState::DumpTransfers(const std::string& filename)
+void GSState::DumpTransferList(const std::string& filename)
 {
 	// Only create the file if there are transfers to dump
 	std::optional<std::ofstream> file;
@@ -792,13 +793,45 @@ void GSState::DumpTransfers(const std::string& filename)
 		(*file) << INDENT << "rect: [" << std::dec << transfer.rect.x << DEL << transfer.rect.y << DEL <<
 			transfer.rect.z << DEL << transfer.rect.w << "]" << std::endl;
 
-		// Dump draw number
-		(*file) << INDENT << "draw: " << std::dec << transfer.draw << std::endl;
-
 		// Dump zero_clear
 		(*file) << INDENT << "zero_clear: " << (transfer.zero_clear ? "true" : "false") << std::endl;
 
 		n_dumped++;
+	}
+}
+
+void GSState::DumpTransferImages()
+{
+	// Only create the file if there are transfers to dump
+	std::optional<std::ofstream> file;
+
+	int transfer_n = 0;
+	for (int i = 0; i < static_cast<int>(m_draw_transfers.size()); ++i)
+	{
+		if (m_draw_transfers[i].draw != s_n - 1)
+			continue; // skip transfers that did not start in the previous draw
+
+		const GSUploadQueue& transfer = m_draw_transfers[i];
+
+		std::string filename;
+		if (transfer.ee_to_gs)
+		{
+			// Transferring EE->GS then only the destination info is relevant.
+			filename = GetDrawDumpPath("%05d_transfer%02d_EE_to_GS_%03x_%d_%s_%d_%d_%d_%d.png",
+				s_n, transfer_n++, transfer.blit.DBP, transfer.blit.DBW, GSUtil::GetPSMName(transfer.blit.DPSM),
+				transfer.rect.x, transfer.rect.y, transfer.rect.z, transfer.rect.w);
+		}
+		else
+		{
+			// Transferring GS->GS then the source info is relevant.
+			filename = GetDrawDumpPath("%05d_transfer%02d_GS_to_GS_%03x_%d_%s_%03x_%d_%s_%d_%d_%d_%d.bmp",
+				s_n, transfer_n++, transfer.blit.SBP, transfer.blit.SBW, GSUtil::GetPSMName(transfer.blit.SPSM),
+				transfer.blit.DBP, transfer.blit.DBW, GSUtil::GetPSMName(transfer.blit.DPSM),
+				transfer.rect.x, transfer.rect.y, transfer.rect.z, transfer.rect.w);
+		}
+
+		m_mem.SaveBMP(filename, transfer.blit.DBP, transfer.blit.DBW, transfer.blit.DPSM,
+			transfer.rect.width(), transfer.rect.height(), transfer.rect.x, transfer.rect.y);
 	}
 }
 
@@ -2029,12 +2062,18 @@ void GSState::FlushPrim()
 		const bool skip_draw = (m_context->TEST.ZTE && m_context->TEST.ZTST == ZTST_NEVER);
 		m_quad_check_valid = false;
 
-		if (GSConfig.SaveInfo && GSConfig.ShouldDump(s_n, g_perfmon.GetFrame()))
+		if (GSConfig.ShouldDump(s_n, g_perfmon.GetFrame()))
 		{
-			// Only dump registers/vertices if we are drawing.
-			// Always dump the transfers since these are relevant for debugging regardless of
-			// whether the draw is skipped or not.
-			DumpDrawInfo(!skip_draw, !skip_draw, true);
+			if (GSConfig.SaveInfo)
+			{
+				// Only dump registers/vertices if we are drawing.
+				// Always dump the transfers since these are relevant for debugging regardless of
+				// whether the draw is skipped or not.
+				DumpDrawInfo(!skip_draw, !skip_draw, true);
+			}
+
+			if (GSConfig.SaveTransferImages)
+				DumpTransferImages();
 		}
 
 		if (!skip_draw)